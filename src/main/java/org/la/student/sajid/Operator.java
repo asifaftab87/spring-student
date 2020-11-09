cde1d094669178a936c4f6bf9e1c2c7a1806d4d2
sjdali819 <37649963+sjdali819@users.noreply.github.com>
2020-11-09 18:47:43 +0530

    first commit by sajid

---
+++ src/main/java/org/la/student/sajid/Operator.java
@@ -0,0 +1,48 @@
+package org.la.student.sajid;
+
+public class Operator {
+
+	public static void main(String[] args) {
+		int i = 19/5;
+		System.out.println(i);
+		
+		char a= 65;
+		System.out.println(a);
+		
+		int s= 4;
+		int p=s++;
+		System.out.println(p);
+		
+		
+		int f=5;
+		int b=3;
+		int c=f+b;
+		System.out.println(c);
+		
+		int g=5;
+		int h=6;
+		int j=8;
+		int k = j+h+g;
+		System.out.println(k);
+		
+		int l=7-9;
+		System.out.println(l);
+		
+		int m=4-5-8;
+		System.out.println(m);
+		
+		int n=5*2;
+		System.out.println(n);
+		
+		int v=5*8*9;
+		System.out.println(v);
+		
+		int x=10%3;
+		System.out.println(x);
+		
+		int z=200/100*5;
+		System.out.println(z);
+		
+	}
+
+}
