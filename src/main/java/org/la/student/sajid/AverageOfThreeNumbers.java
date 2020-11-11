44638a0aa8ebfda993b0112f8e8ffbfc6f7ec16a
sjdali819 <37649963+sjdali819@users.noreply.github.com>
2020-11-11 14:45:15 +0530

    commit by sajid

---
+++ src/main/java/org/la/student/sajid/AverageOfThreeNumbers.java
@@ -0,0 +1,14 @@
+package org.la.student.sajid;
+
+public class AverageOfThreeNumbers {
+
+	public static void main(String[] args) {
+		int a=3;
+		int w=9;
+		int d=29;
+		int f=a+w+d;
+		int s=f/3;
+		System.out.println(s);
+	}
+
+}
