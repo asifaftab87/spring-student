cf1b8ad36cf2f065f3a59c16c00a1953ab271370
DATA <DATA@192.168.1.13>
2020-11-11 18:34:01 +0530

    Decreement Program

---
+++ src/main/java/org/la/student/ameen/PostDecreementSameLine.java
@@ -0,0 +1,11 @@
+package org.la.student.ameen;
+
+public class PostDecreementSameLine {
+
+	public static void main(String[] args) {
+		int h = 56 , f = h-- ;
+		System.out.println(h);
+		System.out.println(f);
+	}
+
+}
---
+++ src/main/java/org/la/student/ameen/PreDecreementMultiply.java
@@ -0,0 +1,13 @@
+package org.la.student.ameen;
+
+public class PreDecreementMultiply {
+
+	public static void main(String[] args) {
+		int j = 85 ;
+		--j;
+		int k = j * 4;
+		System.out.println(j);
+		System.out.println(k);
+	}
+
+}
