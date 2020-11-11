0c81a9499224755623276cf9dd1176d6333075db
DATA <DATA@DATA-PC.domain.name>
2020-11-11 15:31:27 +0530

    variables

---
+++ src/main/java/org/la/student/ameen/CharVariable.java
@@ -0,0 +1,13 @@
+package org.la.student.ameen;
+
+public class CharVariable {
+
+	public static void main(String[] args) 
+	{
+		char ch = 65;
+		System.out.println("ch : " + ch);
+		
+
+	}
+
+}
---
+++ src/main/java/org/la/student/ameen/FloatVariable.java
@@ -0,0 +1,13 @@
+package org.la.student.ameen;
+
+public class FloatVariable {
+
+	public static void main(String[] args) 
+	{
+		float m = 65f;
+		System.out.println("m : " + m);
+		
+
+	}
+
+}
