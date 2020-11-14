467483f154a38415ea90241984dadc98e07015e0
asifaftab87 <asifaftab87@gmail.com>
2020-11-14 23:02:22 +0800

    operators done

---
+++ src/main/java/org/la/student/asif/TernaryOpertor.java
@@ -0,0 +1,14 @@
+package org.la.student.asif;
+
+public class TernaryOpertor {
+
+	public static void main(String[] args) {
+		
+		
+		int x=21, y=40;
+		
+		int r = x>y ? x : y;
+		
+		System.out.println("r: "+r);
+	}
+}
