0782cb04c8f2faec0945a2358021b8c1330a93f4
nourahamdi <73748252+nourahamdi@users.noreply.github.com>
2020-11-14 11:58:17 +0300

    (Decrement and Encrement)

---
+++ src/main/java/org/la/student/noura/DecAndEncOperation.java
@@ -0,0 +1,23 @@
+package org.la.student.noura;
+
+public class DecAndEncOperation {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+		int f = 3;
+		int g = f++ * 6;
+		System.out.println("f="+f);
+		System.out.println("g="+g);
+		
+		
+		int x = 3;
+		int t = x++ / 3;
+		System.out.println("x="+x);
+		System.out.println("t="+t);
+	}
+
+}
