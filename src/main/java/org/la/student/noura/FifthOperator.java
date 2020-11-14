e6b28980822b6832875b3b22723269e6c3459c47
nourahamdi <73748252+nourahamdi@users.noreply.github.com>
2020-11-14 12:22:56 +0300

    percentage.

---
+++ src/main/java/org/la/student/noura/FifthOperator.java
@@ -0,0 +1,20 @@
+package org.la.student.noura;
+
+public class FifthOperator {
+
+	public static void main(String[] args) {
+
+		int a =400;
+		int b =200;
+		int c =40; 
+		int e=(a/b)*c;
+		System.out.println("e="+e);
+		
+		int n=2;
+		int o=9;
+		int q=o/n;
+		System.out.println("q="+q);
+
+	}
+
+}
