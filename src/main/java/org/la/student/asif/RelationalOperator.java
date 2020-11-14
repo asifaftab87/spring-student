e1340bca181e65d9bdb27cc5731d29cf0d1dd659
asifaftab87 <asifaftab87@gmail.com>
2020-11-14 21:54:34 +0800

    relational operator example

---
+++ src/main/java/org/la/student/asif/RelationalOperator.java
@@ -0,0 +1,16 @@
+package org.la.student.asif;
+
+public class RelationalOperator {
+
+	public static void main(String[] args) {
+		
+		int i=11, k=1;
+		
+		boolean r = i != k;
+		System.out.println("r: "+r);
+		
+		r = i < k;
+		System.out.println("r: "+r);
+		
+	}
+}
