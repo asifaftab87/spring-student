9539e1182afcdc58f54a0b905442b20c2441aa05
asifaftab87 <asifaftab87@gmail.com>
2020-11-17 11:58:06 +0800

    commit changes

--- src/main/java/org/la/student/asif/TernaryOpertor2.java
+++ src/main/java/org/la/student/asif/TernaryOpertor2.java
@@ -4,11 +4,11 @@ public class TernaryOpertor2 {
 
 	public static void main(String[] args) {
 		
-		
 		int x=21, y=4, z =32;
 		
 		int r = (x>y) ? (x>z ? x : z) : (y>z ? y : z) ;
 		
 		System.out.println("r: "+r);
+		
 	}
 }
