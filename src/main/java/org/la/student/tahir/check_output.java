f025f58d8a021c4748ff86e1a1ec97657483f22b
Dalori <Dalori@10.211.55.8>
2020-11-17 08:37:16 +0800

    Assignment Update

--- src/main/java/org/la/student/tahir/check_output.java
+++ src/main/java/org/la/student/tahir/check_output.java
@@ -3,8 +3,35 @@ package org.la.student.tahir;
 public class check_output {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		int a=3;
+		int b= ++a;
+		
+		System.out.println(a);
+		System.out.println(b);
+		
+		int c= -3;
+		int d = c++;
+		System.out.println(c);
+		System.out.println(d);
+		
+		int e = 3;
+		int f = ++e + 9 ;
+		System.out.println(e);
+		System.out.println(f);
+		
+		int g = 3;
+		int h = g++ * 6;
+		System.out.println(g);
+		System.out.println(h);
+		
+		int i = 3;
+		int j = i++ /3;
+		System.out.println(i);
+		System.out.println(j);
+		
+		
+		
 	}
 
 }
