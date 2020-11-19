11e7702b062def1026b29c5b126eeedd31063ef8
DATA <DATA@192.168.1.13>
2020-11-19 15:33:34 +0530

    Compare two number by using relational operator

---
+++ src/main/java/org/la/student/ameen/CompareTwoNumber.java
@@ -0,0 +1,12 @@
+package org.la.student.ameen;
+
+public class CompareTwoNumber 
+{
+	public static void main(String[] args) 
+	{
+		int a = 15 , b = 14 ;
+		boolean m = a == b ;
+		System.out.println("m : " + m);
+	}
+
+}
