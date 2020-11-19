87f026e5349e1f3855a7c37174eb4eecaee6fa14
DATA <DATA@192.168.1.13>
2020-11-19 15:49:56 +0530

    Number compared with the operators

---
+++ src/main/java/org/la/student/ameen/CompareNum.java
@@ -0,0 +1,9 @@
+package org.la.student.ameen;
+
+public class CompareNum {
+	public static void main(String[] args) {
+		int l = 34 , j = 54 ;
+		boolean c = j >= l ;
+		System.out.println(" c : " + c);
+	}
+}
