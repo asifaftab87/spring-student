1507f6c43b396f17fb0401f8a8557930153d24b5
nourahamdi <73748252+nourahamdi@users.noreply.github.com>
2020-11-20 18:37:32 +0300

    logical operator

---
+++ src/main/java/org/la/student/noura/Excercise2.java
@@ -0,0 +1,41 @@
+package org.la.student.noura;
+
+public class Excercise2 {
+
+	public static void main(String[] args) {
+
+		
+		
+		
+
+	    int n = 5;
+	    System.out.println(n > 3 && n < 10); // returns true because 5 is greater than 3 AND 5 is less than 10
+	 
+	
+	
+	
+		
+		
+		
+		int a =1;
+		int b =2;
+		if (a==1 || ++b==3);
+		System.out.println("hello world");
+		System.out.println("a="+a);
+		System.out.println("b="+b);
+		
+		
+		
+		
+		
+		
+		int h= 1;
+		int j= 2;
+		if (h!=1 && j++==3);
+		System.out.println("hello world");
+		System.out.println("h="+h);
+		System.out.println("j="+j);
+		
+	}
+
+}
---
+++ src/main/java/org/la/student/noura/Excercise3.java
@@ -0,0 +1,24 @@
+package org.la.student.noura;
+
+public class Excercise3 {
+
+	public static void main(String[] args) {
+
+		int r= 1;
+		int e= 2;
+		if (r!=1 || e++==3);
+		System.out.println("hello world");
+		System.out.println("r="+r);
+		System.out.println("e="+e);
+		
+		int x = 1;
+		int v = 2;
+		if (x==1 && ++v==3);
+		System.out.println("hello world");
+		System.out.println("x="+x);
+		System.out.println("v="+v);
+		
+		
+	}
+
+}
