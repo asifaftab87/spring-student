19f6f7687f811f3f50a634d3bc4d4fb6247d5d50
nourahamdi <73748252+nourahamdi@users.noreply.github.com>
2020-11-20 17:29:21 +0300

    Arithmetic Operators

---
+++ src/main/java/org/la/student/noura/DifferentExcercises.java
@@ -0,0 +1,36 @@
+package org.la.student.noura;
+
+public class DifferentExcercises {
+
+	public static void main(String[] args) {
+		int x=10;
+		int y = -2;
+		int z= ++x*y++;
+		System.out.println("x:"+x);
+		System.out.println("y:"+y);
+		System.out.println("z:"+z);
+
+		
+		int g =10;
+		int f =-2;
+		int s = g++*f++;
+		System.out.println("g:"+ g);
+		System.out.println("f:"+f);
+		System.out.println("s:"+s);
+
+		
+		
+		int r = -1;
+		int a = 2;
+		int h = --r*a++;
+		System.out.println("r:"+r);
+		System.out.println("a:"+a);
+		System.out.println("h:"+h);
+		
+		
+		
+
+		
+	}
+
+}
