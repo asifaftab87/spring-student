f92350708c125f3491ba15e58392f763ceb03ea6
nourahamdi <73748252+nourahamdi@users.noreply.github.com>
2020-11-20 17:36:15 +0300

    Relational Operator

---
+++ src/main/java/org/la/student/noura/Excercise1.java
@@ -0,0 +1,27 @@
+package org.la.student.noura;
+
+public class Excercise1 {
+
+	public static void main(String[] args) {
+
+		
+		int a =10;
+		int b= a-5;
+		System.out.println(a<=b);
+		System.out.println(a>b);
+//  = is assignment operator but == is relational operator called an equality 
+
+
+		int c =10;
+		int d =15;
+		c++;
+		d--;
+		int v =d%c;
+		
+		System.out.println(v>=d);
+		
+		
+
+                             }
+	
+	}
