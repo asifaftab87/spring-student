6bfb8199bc0e82a7702a7a1167add0fea8b7a929
HP <HP@LAPTOP-MDJKCEIE>
2020-11-20 15:20:41 +0800

    first message

--- src/main/java/org/la/student/tania/Boolean_operation.java
+++ src/main/java/org/la/student/tania/Boolean_operation.java
@@ -14,6 +14,51 @@ public class Boolean_operation {
         System.out.println(c);
         System.out.println(d);
         System.out.println(e);
+        
+        boolean lampx=false,result;
+        boolean  lampy=true;
+        result=lampy&&lampx;
+        System.out.println("lamp_switch_on" + result);
+        result = lampy & lampx;
+        System.out.println("lamp_switch_on" +result);
+        
+        boolean lampm=false, add;
+        boolean  lampn=true;
+        add=lampm||lampn;
+        System.out.println("lamp_switch_on" + add);
+        result = lampm | lampn;
+        System.out.println("lamp_switch_on" +add);
+        
+        
+        boolean lampk=false, output;
+        boolean  lampl=false;
+        output=lampk||lampl;
+        System.out.println("lamp_switch_on" + output);
+        output = lampk && lampl;
+        System.out.println("lamp_switch_on" +output);
+        
+        
+        
+        
+        
+        
+       
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
 
 }
 }
