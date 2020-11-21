ac3f864d37ae9a5bae0f87a27ed7f6d8e5af153a
nourahamdi <73748252+nourahamdi@users.noreply.github.com>
2020-11-21 16:30:08 +0300

    Ternary Operator

--- src/main/java/org/la/student/noura/Excercise6.java
+++ src/main/java/org/la/student/noura/Excercise6.java
@@ -4,8 +4,8 @@ public class Excercise6 {
 
 	public static void main(String[] args) {
 
-		double d1 = 21.5;
-		double d2 = 0.5;
+		int d1 = 21;
+		int d2 = 7;
 	    boolean condition =(d1/d2)<15; 
 	    int result = condition ? 77 : 99;
 	    System.out.println("result="+ result);
