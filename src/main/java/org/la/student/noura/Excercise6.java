2060cda5acbdd1decb9098e10055a83ca8dd4657
nourahamdi <73748252+nourahamdi@users.noreply.github.com>
2020-11-21 16:02:44 +0300

    Ternary Operator

---
+++ src/main/java/org/la/student/noura/Excercise6.java
@@ -0,0 +1,15 @@
+package org.la.student.noura;
+
+public class Excercise6 {
+
+	public static void main(String[] args) {
+
+		double d1 = 21.5;
+		double d2 = 0.5;
+	    boolean condition =(d1/d2)<15; 
+	    int result = condition ? 77 : 99;
+	    System.out.println("result="+ result);
+	}
+
+}
+
