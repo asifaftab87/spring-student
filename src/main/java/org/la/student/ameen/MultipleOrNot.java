931db8d41f80387775bd8ba52ed2567ce69035e7
Aameen <Aameen@DATA-PC.domain.name>
2020-11-23 14:01:28 +0530

    Multiple of 10 or not

---
+++ src/main/java/org/la/student/ameen/MultipleOrNot.java
@@ -0,0 +1,18 @@
+package org.la.student.ameen;
+
+public class MultipleOrNot {
+
+	public static void main(String[] args) {
+		int num = 110;
+		if(num % 10 == 0)
+		{
+			System.out.println("It is a Multiple of 10");
+		}
+		else
+		{
+			System.out.println("Not Multiple of 10");
+		}
+
+	}
+
+}
