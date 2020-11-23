c6f196a0454d480fb181f544f2ba938451c660ec
nourahamdi <73748252+nourahamdi@users.noreply.github.com>
2020-11-23 22:40:24 +0300

    ternary inside ternary (nested)

---
+++ src/main/java/org/la/student/noura/TernaryOperator2.java
@@ -0,0 +1,16 @@
+package org.la.student.noura;
+
+public class TernaryOperator2 {
+
+	public static void main(String[] args) {
+
+		int v=5 ; int b= 87 ; int e=89;
+		
+		int g = (v>b)?(v>e?v:e):(b>e?b:e);
+		
+		System.out.println(g);
+		
+		
+	}
+
+}
