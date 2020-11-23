5f215243a0be5fde398c8da7b2d104305cd190a4
Aameen <Aameen@DATA-PC.domain.name>
2020-11-23 16:35:35 +0530

    divisible by 7 or not

---
+++ src/main/java/org/la/student/ameen/Dividing.java
@@ -0,0 +1,17 @@
+package org.la.student.ameen;
+
+public class Dividing {
+
+	public static void main(String[] args) {
+		 int a = 82;
+		 if(a % 7 == 0)
+		 {
+			 System.out.println("it is divisible by 7");
+		 }
+		 else
+		 {
+			 System.out.println("not divisible by 7");
+		 }
+	}
+
+}
