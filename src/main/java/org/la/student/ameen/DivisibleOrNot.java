1b90897d5e2169163db41bf95e2a8aae12cb680e
Aameen <Aameen@DATA-PC.domain.name>
2020-11-23 13:45:05 +0530

    Completely divisible or not

---
+++ src/main/java/org/la/student/ameen/DivisibleOrNot.java
@@ -0,0 +1,17 @@
+package org.la.student.ameen;
+
+public class DivisibleOrNot {
+
+	public static void main(String[] args) {
+		int num = 54 ;
+		if(num % 2 == 0)
+		{
+			System.out.println("It is divisible");
+		}
+		else
+		{
+			System.out.println("it is not divisible");
+		}
+	}
+
+}
