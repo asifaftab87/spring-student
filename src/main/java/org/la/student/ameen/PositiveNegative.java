69d826518a7203af39c79741714fe717b4cc5ba1
Aameen <Aameen@DATA-PC.domain.name>
2020-11-23 12:03:42 +0530

    Positive , negative or zero using if else operator

---
+++ src/main/java/org/la/student/ameen/PositiveNegative.java
@@ -0,0 +1,22 @@
+package org.la.student.ameen;
+
+public class PositiveNegative {
+
+	public static void main(String[] args) {
+		int num = 0 ;
+		if(num > 0)
+		{
+			System.out.println("it is positive");
+		}
+		else if(num == 0)
+		{
+			System.out.println("it is zero");
+		}
+		else
+		{
+			System.out.println("it is negative");
+		}
+
+	}
+
+}
