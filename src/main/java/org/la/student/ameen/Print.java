7886cbe73f925be93431423d06a770cec00844f5
Aameen <Aameen@DATA-PC.domain.name>
2020-11-23 16:46:56 +0530

    hi if number is zero and by if number is 100

---
+++ src/main/java/org/la/student/ameen/Print.java
@@ -0,0 +1,22 @@
+package org.la.student.ameen;
+
+public class Print {
+
+	public static void main(String[] args) {
+		int n = 95 ;
+		if(n == 0)
+		{
+			System.out.println("hi");
+		}
+		else if(n == 100)
+		{
+			System.out.println("by");
+		}
+		else
+		{
+			System.out.println("n is not equal to 0 and 100");
+		}
+
+	}
+
+}
