a4511b6c72f11ef1fab420417877cd796903ef2a
Aameen <Aameen@DATA-PC.domain.name>
2020-11-23 17:08:06 +0530

    Double digit or not

---
+++ src/main/java/org/la/student/ameen/DoubleDigit.java
@@ -0,0 +1,22 @@
+package org.la.student.ameen;
+
+public class DoubleDigit {
+
+	public static void main(String[] args) {
+		int num = -19;
+		if( num >= 10 && num < 100)
+		{
+			System.out.println(" num is double");
+			}
+		else if(num <= -10 && num > -100)
+		{
+			System.out.println("num is double");
+		}
+		else
+		{
+			System.out.println(" num is not double");
+		}
+
+	}
+
+}
