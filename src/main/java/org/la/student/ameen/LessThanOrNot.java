ba6472dd363a34eaf70c1e46807ad8af61599e7a
Aameen <Aameen@DATA-PC.domain.name>
2020-11-23 16:03:09 +0530

    less than 100 or not

---
+++ src/main/java/org/la/student/ameen/LessThanOrNot.java
@@ -0,0 +1,18 @@
+package org.la.student.ameen;
+
+public class LessThanOrNot {
+
+	public static void main(String[] args) {
+		int num = 56;
+		if(num < 100)
+		{
+			System.out.println(" it is less than 100");
+		}
+		else
+		{
+			System.out.println("it is more than 100");
+		}
+
+	}
+
+}
