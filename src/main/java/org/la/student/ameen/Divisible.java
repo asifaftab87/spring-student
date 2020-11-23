278ccd9675ba2b787bf8b1687f3bc58d62c7f541
Aameen <Aameen@DATA-PC.domain.name>
2020-11-23 16:18:35 +0530

    Divisible by 3 or not

---
+++ src/main/java/org/la/student/ameen/Divisible.java
@@ -0,0 +1,18 @@
+package org.la.student.ameen;
+
+public class Divisible {
+
+	public static void main(String[] args) {
+		int n = 92;
+		if(n % 3 == 0)
+		{
+			System.out.println("it is divisible by 3");
+		}
+		else
+		{
+			System.out.println("it is not divisible by 3");
+		}
+
+	}
+
+}
