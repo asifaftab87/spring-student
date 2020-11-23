bbc30a28979726da784beb2cf5260b0cd6e88fd4
Aameen <Aameen@DATA-PC.domain.name>
2020-11-23 11:11:30 +0530

    Finding odd and even with if operator

---
+++ src/main/java/org/la/student/ameen/OddEven.java
@@ -0,0 +1,16 @@
+package org.la.student.ameen;
+
+public class OddEven {
+	public static void main(String[] args) {
+		int num = 56 ;
+		if (num % 2 == 0)
+		{
+			System.out.println("num : is a even " );
+		}
+		else 
+		{
+			System.out.println("num : is a odd num " );
+		}
+	}
+
+}
