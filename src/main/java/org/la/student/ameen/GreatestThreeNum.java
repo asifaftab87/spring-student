b107cc16b0ad21aa3dcb687446f2ce6eea19ca63
Aameen <Aameen@DATA-PC.domain.name>
2020-11-23 14:13:10 +0530

    Greatest number between three number

---
+++ src/main/java/org/la/student/ameen/GreatestThreeNum.java
@@ -0,0 +1,22 @@
+package org.la.student.ameen;
+
+public class GreatestThreeNum {
+
+	public static void main(String[] args) {
+		int num1 = 15 ,num2 = 13 ,num3 = 27;
+		if(num1 > num2 && num1 > num3)
+		{
+			System.out.println("num 1 is greatest num ");
+		}
+		else if (num2 > num1 && num2 > num3)
+		{
+			System.out.println("num2 is greatest num");
+		}
+		else
+		{
+			System.out.println(" num3 is greatest num");
+		}
+
+	}
+
+}
