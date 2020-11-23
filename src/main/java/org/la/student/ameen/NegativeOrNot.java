400ca6b2dff0cbb6debac2acbdad6a2ecca3feb2
Aameen <Aameen@DATA-PC.domain.name>
2020-11-23 14:34:27 +0530

    Negative or not

---
+++ src/main/java/org/la/student/ameen/NegativeOrNot.java
@@ -0,0 +1,18 @@
+package org.la.student.ameen;
+
+public class NegativeOrNot {
+
+	public static void main(String[] args) {
+		int num = 15;
+		if(num < 0)
+		{
+			System.out.println("num  is negative");
+		}
+		else
+		{
+			System.out.println("num is not negative");
+		}
+
+	}
+
+}
