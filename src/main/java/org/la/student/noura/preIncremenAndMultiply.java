9bb20b78cc93421d88cdd1b366a647b9cf4bb615
nourahamdi <73748252+nourahamdi@users.noreply.github.com>
2020-11-24 13:31:08 +0300

    preincrement

---
+++ src/main/java/org/la/student/noura/preIncremenAndMultiply.java
@@ -0,0 +1,17 @@
+package org.la.student.noura;
+
+public class preIncremenAndMultiply {
+
+	public static void main(String[] args) {
+
+		
+		int n=5;
+		++n;
+		int d = n*6;
+		
+		System.out.println(d);
+		System.out.println(n);
+		
+	}
+
+}
