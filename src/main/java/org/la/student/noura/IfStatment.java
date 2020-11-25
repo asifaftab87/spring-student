7f1da4956e49e17798c6e3e66cbe396ae86b4878
nourahamdi <73748252+nourahamdi@users.noreply.github.com>
2020-11-25 18:36:34 +0300

    check the number (odd or even)

---
+++ src/main/java/org/la/student/noura/IfStatment.java
@@ -0,0 +1,15 @@
+package org.la.student.noura;
+
+public class IfStatment {
+
+	public static void main(String[] args) {
+
+		int a= 6;
+		 if (a%2==0) {
+			 System.out.println("this number is even");
+		 }
+              System.out.println("this number is odd");
+		
+	}
+
+}
