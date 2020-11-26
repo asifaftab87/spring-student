ab727c318d485917681bb76f841ca31f12e9c10a
DATA <DATA@DATA-PC.domain.name>
2020-11-27 00:07:07 +0530

    wap to print 'low' if any given character is lower case

---
+++ src/main/java/org/la/student/ameen/LowerClass.java
@@ -0,0 +1,19 @@
+package org.la.student.ameen;
+
+public class LowerClass {
+
+	public static void main(String[] args) {
+		// wap to print 'low' if any given character is lower case
+		char ch = 'h';
+		if(ch > 'a' && ch < 'z')
+		{
+			System.out.println("low");
+		}
+		else 
+		{
+			System.out.println(" given character is not a lower class");
+		}
+
+	}
+
+}
