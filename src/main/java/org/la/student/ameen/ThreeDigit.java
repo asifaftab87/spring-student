72699ff79ff201f1496bea0d05cf240895ea1237
DATA <DATA@DATA-PC.domain.name>
2020-11-26 22:51:36 +0530

    wap to print ternary if number is 3 digit

---
+++ src/main/java/org/la/student/ameen/ThreeDigit.java
@@ -0,0 +1,18 @@
+package org.la.student.ameen;
+
+public class ThreeDigit {
+
+	public static void main(String[] args) {
+		//wap to print ternary if number is 3 digit
+		double n = 100;
+		if (n >= 100 && n < 1000)
+		{
+			System.out.println("Ternary");
+	}
+		else
+		{
+			System.out.println("Not Ternary");
+		}
+	}
+
+}
