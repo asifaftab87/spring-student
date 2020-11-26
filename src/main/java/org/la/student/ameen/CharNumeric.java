c4a064e71d9a10dfcd130bba72207f20494269d8
DATA <DATA@DATA-PC.domain.name>
2020-11-27 00:14:08 +0530

    wap to print 'num' if any given character is char numeric e.g. '1' or any such given number

---
+++ src/main/java/org/la/student/ameen/CharNumeric.java
@@ -0,0 +1,20 @@
+package org.la.student.ameen;
+
+public class CharNumeric {
+
+	public static void main(String[] args) {
+		//  wap to print 'num' if any given character is char numeric e.g. '1' or any such given number
+		char ch = '1' ;
+		if(ch >= 1)
+		{
+			System.out.println("num");
+		}
+		else
+		{
+			System.out.println("no num");
+		}
+			
+
+	}
+
+}
