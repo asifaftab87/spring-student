1965e6158fd2b6676fd65c19a5a32a178c9921bb
DATA <DATA@DATA-PC.domain.name>
2020-11-27 00:26:48 +0530

    . wap to check whether a given number is positive and double digit or single digit number

---
+++ src/main/java/org/la/student/ameen/CheckNum.java
@@ -0,0 +1,27 @@
+package org.la.student.ameen;
+
+public class CheckNum {
+
+	public static void main(String[] args) {
+		// . wap to check whether a given number is positive and double digit or single digit number 
+		int num =  5 ;
+		if(num >= 0)
+		{
+			System.out.println("number is Positive");
+		}
+		 if(num>10 && num < 100)
+		{
+			System.out.println("number is double digit ");
+		}
+		else if(num > 1 && num < 10 )
+		{
+		System.out.println("number is single digit");	
+		}
+		else 
+		{
+			System.out.println(" given number is not single digit nor positive nor double digit");
+		}
+
+	}
+
+}
