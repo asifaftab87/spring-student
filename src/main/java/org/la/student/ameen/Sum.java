83a7b63ecb580a7ed5310e576733090cc3ce9b2d
DATA <DATA@DATA-PC.domain.name>
2020-11-26 22:57:39 +0530

    wap to print 100 if number is between 100 to 200

---
+++ src/main/java/org/la/student/ameen/Sum.java
@@ -0,0 +1,18 @@
+package org.la.student.ameen;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// wap to print 100 if number is between 100 to 200
+		double n = 174;
+		if(n >=100 && n <=200)
+		{
+			System.out.println("100");
+		}
+		else 
+		{
+			System.out.println("Enter the number between 100 to 200");
+		}
+	}
+
+}
