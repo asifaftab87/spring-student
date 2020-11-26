065f5b38ca2904126d3e33bb558a2ace089c218e
DATA <DATA@DATA-PC.domain.name>
2020-11-26 23:40:44 +0530

    wap to find least number among two numbers

---
+++ src/main/java/org/la/student/ameen/LeastNumber.java
@@ -0,0 +1,19 @@
+package org.la.student.ameen;
+
+public class LeastNumber {
+
+	public static void main(String[] args) {
+		// wap to find least number among two numbers
+		int sum1 = 15 , sum2 = 34;
+		if(sum1 < sum2 )
+		{
+			System.out.println("sum1 is the least number");
+		}
+		else
+		{
+			System.out.println("sum2 is the least number");
+		}
+
+	}
+
+}
