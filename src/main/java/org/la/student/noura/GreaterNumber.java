816b72830ef3ee57a1e6365be037a06a1c36c663
nourahamdi <73748252+nourahamdi@users.noreply.github.com>
2020-11-27 20:59:22 +0300

    finding the greatest number

---
+++ src/main/java/org/la/student/noura/GreaterNumber.java
@@ -0,0 +1,30 @@
+package org.la.student.noura;
+
+public class GreaterNumber {
+
+	public static void main(String[] args) {
+
+		
+		int a= 65;
+		int b= 100;
+		int c= 500;
+	 
+		
+		if(a > b && a > c)
+		{
+			System.out.println(a+" is greatest number ");
+		}
+		else if (b > a && b > c)
+		{
+			System.out.println(b+" is greatest number");
+		}
+		else
+		{
+			System.out.println(c+" is greatest number");
+		}
+		
+		
+		
+	}
+
+}
