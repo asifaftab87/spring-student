f5e66ac9b10b9980aea61ea06da55202ec3fc424
Tania32855 <HP@LAPTOP-MDJKCEIE>
2020-11-28 10:48:58 +0800

    control statement 2

---
+++ src/main/java/org/la/student/tania/Control_statement_11.java
@@ -0,0 +1,21 @@
+package org.la.student.tania;
+
+public class Control_statement_11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        
+		int marks =80;
+		if(marks>80) {
+			System.out.println("Distinction");
+		}
+		if(marks>35) {
+			System.out.println("pass");
+		}
+		else {
+			System.out.println("Fail");
+			System.out.println("Best of luck for next time");
+		}
+	}
+
+}
---
+++ src/main/java/org/la/student/tania/Control_statement_13.java
@@ -0,0 +1,23 @@
+package org.la.student.tania;
+
+public class Control_statement_13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 // wap to check whether a given character is lower case (a toz)or upper case(A to Z)
+		
+		char alpha = 'A';
+
+		if(alpha >='A' && alpha <='Z')
+		{
+		System.out.println("Upper case Alphabet");
+		}
+		else if(alpha >= 'a' && alpha <= 'z')
+		{
+		System.out.println("Lower case Alphabet");
+		}
+
+	}
+
+}
