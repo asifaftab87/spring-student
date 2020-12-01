2c73c126a8e33874a50b16deab03416f8a4e280f
DATA <DATA@DATA-PC.domain.name>
2020-12-02 02:19:27 +0530

    wap to print 10 times wolrd.

---
+++ src/main/java/org/la/student/ameen/World.java
@@ -0,0 +1,16 @@
+package org.la.student.ameen;
+
+public class World {
+
+	public static void main(String[] args) {
+		//wap to print 10 times wolrd.
+		int n = 1;
+		while(n<=10)
+		{
+			System.out.println( n + " . World ");
+			n++;
+		}
+
+	}
+
+}
