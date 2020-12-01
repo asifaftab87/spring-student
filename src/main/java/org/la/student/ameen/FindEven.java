1dd9db1a948667674baece4a20be8fe989420760
DATA <DATA@DATA-PC.domain.name>
2020-12-02 02:25:22 +0530

    wap to print all even numbers between 100 to 120.

---
+++ src/main/java/org/la/student/ameen/FindEven.java
@@ -0,0 +1,19 @@
+package org.la.student.ameen;
+
+public class FindEven {
+
+	public static void main(String[] args) {
+		//wap to print all even numbers between 100 to 120.
+		int i = 100;
+		while(i<=200)
+		{
+			if(i % 2 == 0)
+			{
+				System.out.println(" i : " +i);
+			}
+			i++;
+		}
+
+	}
+
+}
