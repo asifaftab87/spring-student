177543e446376900f8f1184bde81556562b42a04
DATA <DATA@DATA-PC.domain.name>
2020-12-02 02:21:56 +0530

    wap to print -10 to -1.

---
+++ src/main/java/org/la/student/ameen/PrintinMinus.java
@@ -0,0 +1,16 @@
+package org.la.student.ameen;
+
+public class PrintinMinus {
+
+	public static void main(String[] args) {
+		//wap to print -10 to -1.
+		int num = -10;
+		while(num <=-1)
+		{
+			System.out.println("num : " +num);
+			num++;
+		}
+
+	}
+
+}
