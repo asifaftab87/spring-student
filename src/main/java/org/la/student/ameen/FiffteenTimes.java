2abe99947d38c839ef88230c50b3d3eec8d4ff8f
DATA <DATA@DATA-PC.domain.name>
2020-12-02 02:15:09 +0530

    wap to print 15 times hello.

---
+++ src/main/java/org/la/student/ameen/FiffteenTimes.java
@@ -0,0 +1,17 @@
+package org.la.student.ameen;
+
+public class FiffteenTimes {
+
+	public static void main(String[] args) {
+		// wap to print 15 times hello.
+		int n = 1;
+		while(n<=15)
+		{
+			System.out.println(" Hello "  + n);
+			n++;
+		}
+		
+
+	}
+
+}
