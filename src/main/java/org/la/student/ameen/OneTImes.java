8355e4a154cf3b1840b66e51e39bc8a17162eff8
DATA <DATA@DATA-PC.domain.name>
2020-12-02 02:12:20 +0530

    wap to print 1 time a

---
+++ src/main/java/org/la/student/ameen/OneTImes.java
@@ -0,0 +1,16 @@
+package org.la.student.ameen;
+
+public class OneTImes {
+
+	public static void main(String[] args) {
+		// wap to print 1 time a
+		int q = 1;
+		while(q<=1)
+		{
+			System.out.println("a");
+			q++;
+		}
+
+	}
+
+}
