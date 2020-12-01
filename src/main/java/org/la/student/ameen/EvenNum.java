797bf8f8f1ec0d40ce1bc9576b8144187bb0c107
DATA <DATA@DATA-PC.domain.name>
2020-12-02 01:46:10 +0530

    wap to print first five even numbers.

---
+++ src/main/java/org/la/student/ameen/EvenNum.java
@@ -0,0 +1,15 @@
+package org.la.student.ameen;
+
+public class EvenNum {
+
+	public static void main(String[] args) {
+		// wap to print first five even numbers.
+		 int num = 2;
+		 while(num<=10)
+		 {
+			 System.out.println("num :" +num);
+			 num= num +2;
+		 }
+	
+	}
+}
