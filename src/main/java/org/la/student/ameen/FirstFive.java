82b83f5bbb7d27b8c2a39b6ca1c390fbb9d677b7
DATA <DATA@DATA-PC.domain.name>
2020-12-02 01:30:29 +0530

    wap to print first five odd numbers.

---
+++ src/main/java/org/la/student/ameen/FirstFive.java
@@ -0,0 +1,16 @@
+package org.la.student.ameen;
+
+public class FirstFive {
+
+	public static void main(String[] args) {
+		//wap to print first five odd numbers.
+		int num = 1;
+		while(num <= 10)
+		{
+			System.out.println("num : " +num);
+			num = num +2;
+		}
+
+	}
+
+}
