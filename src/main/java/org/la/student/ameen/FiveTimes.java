5dcb88d0b8f466ebbdfd87c270b4d31c4aaa647f
DATA <DATA@DATA-PC.domain.name>
2020-12-02 02:09:44 +0530

    wap to print 5 times A.

---
+++ src/main/java/org/la/student/ameen/FiveTimes.java
@@ -0,0 +1,16 @@
+package org.la.student.ameen;
+
+public class FiveTimes {
+
+	public static void main(String[] args) {
+		//wap to print 5 times A.
+		int i = 1 ;
+		while(i <=5)
+		{
+			System.out.println(" A");
+			i++;
+		}
+
+	}
+
+}
