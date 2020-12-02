d62f3ff5cd5b6fa7f78443d34cce826ecf30ea4b
DATA <DATA@192.168.1.13>
2020-12-02 17:21:28 +0530

    wap to print greatest number among two given numbers with if .

---
+++ src/main/java/org/la/student/ameen/GreatestNumber.java
@@ -0,0 +1,18 @@
+package org.la.student.ameen;
+
+public class GreatestNumber {
+
+	public static void main(String[] args) {
+		// wap to print greatest number among two given numbers with if .
+		int num1 = 1 , num2 = 3;
+		if(num1>num2)
+		{
+			System.out.println("num1 : the greatest number");
+		}
+		else 
+		{
+			System.out.println(" num2 : the greatest number");
+		}
+	}
+
+}
