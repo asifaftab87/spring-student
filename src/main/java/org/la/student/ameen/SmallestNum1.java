4272868da8919ef3658596e96afd9e91c83f84cb
DATA <DATA@192.168.1.13>
2020-12-02 17:33:24 +0530

    wap to print smallest number among two given numbers(  without if).

---
+++ src/main/java/org/la/student/ameen/SmallestNum1.java
@@ -0,0 +1,13 @@
+package org.la.student.ameen;
+
+public class SmallestNum1 {
+
+	public static void main(String[] args) {
+		// wap to print smallest number among two given numbers( without if).
+		int a = 13 , b = 15 ;
+		int small = a < b ? a : b ;
+		System.out.println(" small : " +small);
+
+	}
+
+}
