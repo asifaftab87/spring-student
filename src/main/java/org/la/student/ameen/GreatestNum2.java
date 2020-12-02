c9b16130c751a59b75d0f9e073787887d8ea0037
DATA <DATA@192.168.1.13>
2020-12-02 17:25:11 +0530

    wap to print greatest number among two given numbers(  without if).

---
+++ src/main/java/org/la/student/ameen/GreatestNum2.java
@@ -0,0 +1,13 @@
+package org.la.student.ameen;
+
+public class GreatestNum2 {
+
+	public static void main(String[] args) {
+		// wap to print greatest number among two given numbers without if
+		int a = 15 , b = 25;
+		int big = a > b ? a : b;
+		System.out.println(" big : " +big);
+		
+	}
+
+}
