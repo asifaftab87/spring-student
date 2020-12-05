cceebfd51ad612e830f4e7e152d2f1edd89ff785
DATA <DATA@DATA-PC.domain.name>
2020-12-05 13:20:39 +0530

    wap to print all lower case alphabets.

---
+++ src/main/java/org/la/student/ameen/LowerCase.java
@@ -0,0 +1,15 @@
+package org.la.student.ameen;
+
+public class LowerCase {
+
+	public static void main(String[] args) {
+		// wap to print all lower case alphabets.
+		char ch='a';
+		while(ch<='z'){
+		System.out.println("ch : " +ch);
+		
+		ch++;
+		}
+	}
+
+}
