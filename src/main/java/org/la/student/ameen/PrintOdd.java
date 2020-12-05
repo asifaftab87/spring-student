9b6213a74041ab1e15564302186b1ebe20a77b1c
DATA <DATA@DATA-PC.domain.name>
2020-12-05 17:19:18 +0530

    wap to print odd number among two given numbers( with if ).

---
+++ src/main/java/org/la/student/ameen/PrintOdd.java
@@ -0,0 +1,19 @@
+package org.la.student.ameen;
+
+public class PrintOdd {
+
+	public static void main(String[] args) {
+		// wap to print odd number among two given numbers( with if ).
+		int num1 = 36 , num2 = 21;
+		if(num1%2!=0)
+		{
+			System.out.println("num1 is the odd number");
+		}
+		else 
+		{
+			System.out.println("num2 is the odd number");
+		}
+
+	}
+
+}
