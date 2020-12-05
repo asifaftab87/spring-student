230449e4110b886ca5f61d89276e522e9f0c7dcc
DATA <DATA@DATA-PC.domain.name>
2020-12-05 13:24:42 +0530

    wap to print all upper case alphabets.

---
+++ src/main/java/org/la/student/ameen/UpperCase1.java
@@ -0,0 +1,16 @@
+package org.la.student.ameen;
+
+public class UpperCase1 {
+
+	public static void main(String[] args) {
+		// wap to print all upper case alphabets.
+		char ch ='A';
+		while(ch <='Z')
+		{
+			System.out.println("ch : " +ch);
+		ch++;
+		}
+
+	}
+
+}
