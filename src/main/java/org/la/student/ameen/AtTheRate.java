7ed487bba1b94b0c3505ea4b0dcd5471c3b9e63e
DATA <DATA@DATA-PC.domain.name>
2020-12-05 12:54:06 +0530

    wap to print 'at the rate' if the given character is @ else print 'no rate'

---
+++ src/main/java/org/la/student/ameen/AtTheRate.java
@@ -0,0 +1,20 @@
+package org.la.student.ameen;
+
+public class AtTheRate {
+
+	public static void main(String[] args) {
+		//wap to print 'at the rate' if the given character is @ else print 'no rate'
+		char ch='@';
+		if(ch == '@')
+		{
+			System.out.println("at the rate");
+		
+		}
+		else
+		{
+			System.out.println("no rate");
+		}
+
+	}
+
+}
