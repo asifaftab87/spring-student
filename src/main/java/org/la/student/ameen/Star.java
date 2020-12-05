08a5f1a668bb9dc45371a20f835089ab14096a67
DATA <DATA@DATA-PC.domain.name>
2020-12-05 12:47:10 +0530

    wap to print 'star' if the given character is * else print 'no star'

---
+++ src/main/java/org/la/student/ameen/Star.java
@@ -0,0 +1,19 @@
+package org.la.student.ameen;
+
+public class Star {
+
+	public static void main(String[] args) {
+		// wap to print 'star' if the given character is * else print 'no star'
+		char ch = '*';
+		if(ch == '*')
+		{
+			System.out.println("Star");
+		}
+		else
+		{
+			System.out.println("No star");
+		}
+
+	}
+
+}
