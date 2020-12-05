f0c3dc2915da27ffbc75777fdc5ddf04d66df16e
DATA <DATA@DATA-PC.domain.name>
2020-12-05 12:43:01 +0530

    wap to print 'number' if the given character is number else 'non number'

---
+++ src/main/java/org/la/student/ameen/NumberOrNot.java
@@ -0,0 +1,18 @@
+package org.la.student.ameen;
+
+public class NumberOrNot {
+
+	public static void main(String[] args) {
+		// wap to print 'number' if the given character is number else 'non number'
+		 int num =-11;
+		 if(num >= 1)
+		 {
+			 System.out.println("Number");
+		 }
+		 else
+		 {
+			 System.out.println("Non-number");
+		 }
+	}
+
+}
