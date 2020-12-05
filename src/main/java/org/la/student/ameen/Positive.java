226dab644c69ca93abc639fbe9cf7ee64a59153b
DATA <DATA@DATA-PC.domain.name>
2020-12-05 16:47:53 +0530

    wap to print positive number among two given numbers( with if ).

---
+++ src/main/java/org/la/student/ameen/Positive.java
@@ -0,0 +1,19 @@
+package org.la.student.ameen;
+
+public class Positive {
+
+	public static void main(String[] args) {
+		// wap to print positive number among two given numbers( with if ).
+		int num1 = 87 , num2 = -14;
+		if(num1>=0)
+		{
+			System.out.println("num1 is the positive number");
+		}
+		else 
+		{
+			System.out.println("num2 is the positive number ");
+		}
+
+	}
+
+}
