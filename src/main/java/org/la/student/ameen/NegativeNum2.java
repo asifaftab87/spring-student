343b4f335890da2b9a8dac69c3ec09f88b8a2739
DATA <DATA@DATA-PC.domain.name>
2020-12-05 13:08:00 +0530

    wap to print negative number among two given numbers( without if )

---
+++ src/main/java/org/la/student/ameen/NegativeNum2.java
@@ -0,0 +1,14 @@
+package org.la.student.ameen;
+
+public class NegativeNum2 {
+
+	public static void main(String[] args) {
+		// wap to print negative number among two given numbers( without if ).
+		int a=1, b=-1;
+		int negativeNum = a<0? a : (b<0 ? b : 0) ;
+		System.out.println("negative Num : " +negativeNum);
+		
+
+	}
+
+}
