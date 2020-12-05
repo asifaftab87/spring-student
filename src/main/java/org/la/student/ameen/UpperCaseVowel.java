bcbb97e95f2c58046429414212754d4a283c9499
DATA <DATA@DATA-PC.domain.name>
2020-12-05 13:39:22 +0530

    wap to print all upper case vowels

---
+++ src/main/java/org/la/student/ameen/UpperCaseVowel.java
@@ -0,0 +1,21 @@
+package org.la.student.ameen;
+
+public class UpperCaseVowel {
+
+	public static void main(String[] args) {
+		// wap to print all upper case vowels.
+		char ch ='A';
+		while(ch <='Z')
+		{
+			if(ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U')
+			{
+				System.out.println("ch : " +ch);
+			
+			}
+			ch++;
+		}
+		
+
+	}
+
+}
