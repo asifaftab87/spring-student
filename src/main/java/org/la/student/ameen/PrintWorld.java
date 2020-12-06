5b25fff1566338ffec136e3de77be38f73a93e3d
DATA <DATA@DATA-PC.domain.name>
2020-12-07 00:45:30 +0530

    wap to print 10 times world.(using for loop)

---
+++ src/main/java/org/la/student/ameen/PrintWorld.java
@@ -0,0 +1,15 @@
+package org.la.student.ameen;
+
+public class PrintWorld {
+
+	public static void main(String[] args) {
+		// wap to print 10 times world.(using for loop)
+		int num =1;
+		for(int i = num; i<=10;i++)
+		{
+			System.out.println("World");
+		}
+
+	}
+
+}
