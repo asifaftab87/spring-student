2b2112781caf668de7fd8dcae8ae6d43aa7f6359
DATA <DATA@DATA-PC.domain.name>
2020-12-06 23:52:49 +0530

    wap to print number from 10 to 1(using for loop)

---
+++ src/main/java/org/la/student/ameen/PrintReverseNum.java
@@ -0,0 +1,15 @@
+package org.la.student.ameen;
+
+public class PrintReverseNum {
+
+	public static void main(String[] args) {
+		// wap to print number from 10 to 1(using for loop)
+		int n = 10;
+		for(int i = n ; i>=1 ; i--)
+		{
+			System.out.println("i : " +i);
+		}
+
+	}
+
+}
