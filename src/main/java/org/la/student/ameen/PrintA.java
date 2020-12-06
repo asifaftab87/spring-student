98b4ce87d1509639d047e9fab78543342a46e9c5
DATA <DATA@DATA-PC.domain.name>
2020-12-07 00:37:12 +0530

    wap to print 5 times A.(using for loop)

---
+++ src/main/java/org/la/student/ameen/PrintA.java
@@ -0,0 +1,15 @@
+package org.la.student.ameen;
+
+public class PrintA {
+
+	public static void main(String[] args) {
+		// wap to print 5 times A.(using for loop)
+		char ch = 'A';
+		for(int i = 1 ; i<=5 ; i++)
+		{
+			System.out.println(ch);
+		}
+
+	}
+
+}
