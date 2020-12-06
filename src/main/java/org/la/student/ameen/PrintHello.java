6f26d5ab27d59ad0a8697d9d52887d16a5a5c262
DATA <DATA@DATA-PC.domain.name>
2020-12-07 00:41:05 +0530

    wap to print 15 times hello.(using for loop)

---
+++ src/main/java/org/la/student/ameen/PrintHello.java
@@ -0,0 +1,15 @@
+package org.la.student.ameen;
+
+public class PrintHello {
+
+	public static void main(String[] args) {
+		// wap to print 15 times hello.(using for loop)
+		int num = 1;
+		for(int i= num ; i<=15; i++)
+		{
+			System.out.println("hello");
+		}
+
+	}
+
+}
