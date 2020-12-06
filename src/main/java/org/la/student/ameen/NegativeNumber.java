01a312f7bfcaefa5cc1a822990440fff37041bd9
DATA <DATA@DATA-PC.domain.name>
2020-12-07 00:14:01 +0530

    wap to print to print all negative numbers between -1 and -10.(using for loop)

---
+++ src/main/java/org/la/student/ameen/NegativeNumber.java
@@ -0,0 +1,15 @@
+package org.la.student.ameen;
+
+public class NegativeNumber {
+
+	public static void main(String[] args) {
+		// wap to print to print all negative numbers between -1 and -10.(using for loop)
+		int num = -1;
+		for(int i = num; i>= -10; i--)
+		{
+			System.out.println("i : "+i);
+		}
+
+	}
+
+}
