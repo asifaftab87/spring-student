4be4d76cb10c3cf2a236729de173616e75a97fbb
DATA <DATA@DATA-PC.domain.name>
2020-12-06 23:58:32 +0530

    wap to print first five odd numbers.(using for loop)

---
+++ src/main/java/org/la/student/ameen/FirstFiveOdd.java
@@ -0,0 +1,18 @@
+package org.la.student.ameen;
+
+public class FirstFiveOdd {
+
+	public static void main(String[] args) {
+		// wap to print first five odd numbers.(using for loop)
+		int num = 1;
+		for(int i = num; i<=10; i++)
+		{
+			if(i%2!=0)
+			{
+				System.out.println("i : " +i );
+			}
+		}
+
+	}
+
+}
