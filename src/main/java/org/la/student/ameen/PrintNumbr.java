63ef8f16b6b96de6dcbcae74049bb95649030180
DATA <DATA@DATA-PC.domain.name>
2020-12-06 23:50:05 +0530

    wap to print number from 1 to 10(using for loop)

---
+++ src/main/java/org/la/student/ameen/PrintNumbr.java
@@ -0,0 +1,16 @@
+package org.la.student.ameen;
+
+public class PrintNumbr {
+
+	public static void main(String[] args) {
+		// wap to print number from 1 to 10(using for loop)
+		int num = 1;
+		for(int i = num ; i<=10; i++)
+		{
+			System.out.println("i : " +i);
+		}
+		
+
+	}
+
+}
