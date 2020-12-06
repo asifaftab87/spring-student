26ebc4cb9be2d75020ab37e0a654c47cbb01f651
DATA <DATA@DATA-PC.domain.name>
2020-12-07 00:28:29 +0530

    wap to print numbers which is multiple of 3 between 1 to 22.(using for loop)

---
+++ src/main/java/org/la/student/ameen/MultipleofThree.java
@@ -0,0 +1,18 @@
+package org.la.student.ameen;
+
+public class MultipleofThree {
+
+	public static void main(String[] args) {
+		// wap to print numbers which is multiple of 3 between 1 to 22.(using for loop)
+		int num = 1;
+		for(int i = num; i<=22; i++)
+		{
+			if(i%3==0)
+			{
+				System.out.println(" i : " +i);
+			}
+		}
+
+	}
+
+}
