169d2b4e3657f0c0abf0fd4a09b2c6111febca1f
sjdali819 <37649963+sjdali819@users.noreply.github.com>
2020-12-07 22:27:11 +0530

    by sadique

--- src/main/java/org/la/student/sadique/Print1To10Excepet2And5And8.java
+++ src/main/java/org/la/student/sadique/Print1To10Excepet2And5And8.java
@@ -3,7 +3,7 @@ package org.la.student.sadique;
 public class Print1To10Excepet2And5And8 {
 
 	public static void main(String[] args) {
-		// 38.	wap to print all numbers from 1 to 10 except 2 and 5 and 8.
+//	    38.	wap to print all numbers from 1 to 10 except 2 and 5 and 8.
 		int a=1;
 		while(a<=10) {
 			if(!(a==2||a==5||a==8)) {
@@ -13,5 +13,7 @@ public class Print1To10Excepet2And5And8 {
 		}
 
 	}
+	
+	
 
 }
