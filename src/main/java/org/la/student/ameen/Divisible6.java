980dc7a81802751340facd2b9e4c11c817b06f55
DATA <DATA@DATA-PC.domain.name>
2020-12-12 00:40:39 +0530

    wap to print all numbers between 1 to 150 multiple of 11.(using for loop)

---
+++ src/main/java/org/la/student/ameen/Divisible6.java
@@ -0,0 +1,18 @@
+package org.la.student.ameen;
+
+public class Divisible6 {
+
+	public static void main(String[] args) {
+		// wap to print all numbers between 1 to 150 multiple of 11.(using for loop)
+		int num = 1;
+		for(int i=num ; i<=150;i++)
+		{
+			if(i%11==0)
+			{
+				System.out.println("i : " +i);
+			}
+		}
+
+	}
+
+}
