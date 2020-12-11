f3a9f59c1a647c1bed06436bf18df38cabd20dd5
DATA <DATA@DATA-PC.domain.name>
2020-12-11 14:37:48 +0530

    wap to print -10 to -1.(using for loop)

---
+++ src/main/java/org/la/student/ameen/PrintBackward.java
@@ -0,0 +1,16 @@
+package org.la.student.ameen;
+
+public class PrintBackward {
+
+	public static void main(String[] args) {
+		// wap to print -10 to -1.(using for loop)
+		int num = -10;
+		for(int i = num ; i<=-1; i++)
+		{
+			System.out.println("i :" +i);
+		}
+		
+
+	}
+
+}
