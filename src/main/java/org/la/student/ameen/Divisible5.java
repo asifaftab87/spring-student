15e5abd8ecee57761edd65f90025a27a8bf16520
DATA <DATA@DATA-PC.domain.name>
2020-12-12 00:34:25 +0530

    wap to print all numbers between 100 to 1 those are divisible by 11 and 3.(using for loop)

---
+++ src/main/java/org/la/student/ameen/Divisible5.java
@@ -0,0 +1,18 @@
+package org.la.student.ameen;
+
+public class Divisible5 {
+
+	public static void main(String[] args) {
+		// wap to print all numbers between 100 to 1 those are divisible by 11 and 3.(using for loop)
+		int num=100;
+		for(int i = num ; i>=1;i--)
+		{
+			if(i%3==0||i%11==0)
+			{
+				System.out.println("i : " +i);
+			}
+		}
+
+	}
+
+}
