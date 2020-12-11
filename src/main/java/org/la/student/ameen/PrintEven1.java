4a4a644d18165c3a5fc440f4e547f3d25610a7f1
DATA <DATA@DATA-PC.domain.name>
2020-12-11 14:44:23 +0530

    wap to print all even numbers between 100 to 120.(using for loop)

---
+++ src/main/java/org/la/student/ameen/PrintEven1.java
@@ -0,0 +1,18 @@
+package org.la.student.ameen;
+
+public class PrintEven1 {
+
+	public static void main(String[] args) {
+		// wap to print all even numbers between 100 to 120.(using for loop)
+		int num = 100;
+		for(int i = num; i<=120 ; i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println("i : " +i );
+			}
+		}
+
+	}
+
+}
