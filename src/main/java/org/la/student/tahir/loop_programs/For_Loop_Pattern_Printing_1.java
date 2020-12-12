d41dde462422ad4b00df601a9896c72a3b472b53
Dalori <Dalori@10.211.55.8>
2020-12-12 23:51:11 +0800

    Pattern Printing (increase star)

---
+++ src/main/java/org/la/student/tahir/loop_programs/For_Loop_Pattern_Printing_1.java
@@ -0,0 +1,17 @@
+package org.la.student.tahir.loop_programs;
+
+public class For_Loop_Pattern_Printing_1 {
+
+	public static void main(String[] args) {
+		for (int i=1; i<=8; i++) {
+			
+			for(int k=1; k<=i; k++) {
+				System.out.print('*');
+			}
+			System.out.println("");
+			
+		}
+
+	}
+
+}
