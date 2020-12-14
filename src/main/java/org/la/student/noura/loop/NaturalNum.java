e632ffe4a27059dfb8421b05376afef9f0901933
nourahamdi <73748252+nourahamdi@users.noreply.github.com>
2020-12-14 19:37:46 +0300

    done by noura

---
+++ src/main/java/org/la/student/noura/loop/NaturalNum.java
@@ -0,0 +1,29 @@
+package org.la.student.noura.loop;
+
+public class NaturalNum {
+
+	public static void main(String[] args) {
+		//Wap to print all natural numbers in reverse (from n to 1).
+		//using while loop
+		
+		int i=1;
+		int n=12;
+		while(n>=1) {
+			
+			System.out.print(n+" ");
+			n--;	
+		}
+	
+	
+	System.out.println("                   ");
+	
+	//using for loop
+	
+	for (int a=1,num=13;num>=a;) {
+		
+		System.out.print(num+" ");
+		
+		 num--;
+	   }
+	}
+}
