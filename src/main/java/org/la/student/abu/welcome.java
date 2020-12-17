2bc9ac0173c832da60f4c121435ac67e8ba0c4d7
Dell <Dell@DESKTOP-P4S8DMH>
2020-12-17 21:53:11 +0800

    welcome program

---
+++ src/main/java/org/la/student/abu/welcome.java
@@ -0,0 +1,10 @@
+package org.la.student.abu;
+
+public class welcome {
+
+	public static void main(String[] args) {
+		System.out.println("WELCOME TO ASIF JAVA DEVELOPERS ACADEMY PVT LTD");
+
+	}
+
+}
