f66185a906584857530b79f9dac7e1e9b03b91b6
Dell <Dell@DESKTOP-P4S8DMH>
2020-12-18 15:01:59 +0800

    forloop,rev,prime

---
+++ src/main/java/org/la/student/abu/for_loop04.java
@@ -0,0 +1,17 @@
+package org.la.student.abu;
+
+public class for_loop04 {
+
+	public static void main(String[] args) {
+
+		//To print number from 1 to 10
+
+
+		for (int a= 1;a<=10;a++) {
+
+			System.out.println(a);
+
+		}
+	}
+
+}
