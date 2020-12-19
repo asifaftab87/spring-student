1781bb2697cb520e29199592677fdb0238feefcb
DATA <DATA@DATA-PC.domain.name>
2020-12-19 16:02:22 +0530

    Wap to print all ASCII character with their values.

---
+++ src/main/java/org/la/student/ameen/ASCIIcharacter.java
@@ -0,0 +1,14 @@
+package org.la.student.ameen;
+
+public class ASCIIcharacter {
+
+	public static void main(String[] args) {
+		// Wap to print all ASCII character with their values.
+		for(int i=0;i<=255;i++)
+		{
+			System.out.println("The ASCII character " +(char)i + " = " +i);
+		}
+
+	}
+
+}
