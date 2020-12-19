45c75592cdfd86d3b540caf6d8e3c62f2ccfd174
DATA <DATA@DATA-PC.domain.name>
2020-12-19 13:55:51 +0530

    Sum of all the number from 1 to n using for loop

---
+++ src/main/java/org/la/student/ameen/SumOfAllNumber.java
@@ -0,0 +1,16 @@
+package org.la.student.ameen;
+
+public class SumOfAllNumber {
+
+	public static void main(String[] args) {
+		// Sum of all the number from 1 to n using for loop
+		int n =5;
+		int num=1;
+		for(int i = n; i>=1;i--)
+		{
+			num=num+i;
+		}
+		System.out.println(" num : " +num);
+	}
+
+}
