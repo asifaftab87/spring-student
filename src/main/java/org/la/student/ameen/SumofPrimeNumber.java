c1901a2611219e35bb8a833b72be45c3de46a1dc
DATA <DATA@DATA-PC.domain.name>
2020-12-19 17:18:09 +0530

    Wap to find sum of all prime numbers between 1 to n.

---
+++ src/main/java/org/la/student/ameen/SumofPrimeNumber.java
@@ -0,0 +1,33 @@
+package org.la.student.ameen;
+
+public class SumofPrimeNumber {
+
+	public static void main(String[] args) {
+		// Wap to find sum of all prime numbers between 1 to n.
+		int n =10;
+        int sum = 0;
+
+        for(int num = 2; num <= n; num++) {
+
+            int i;
+
+            for(i = 2; i <= (num / 2); i++) {
+
+                if(num % i == 0) {
+                   
+                	i = num;
+                    break;
+                }
+            }
+
+            
+            if(i != num) {
+            	System.out.print(" ," +num);
+                sum += num;            
+            }
+        }
+
+        System.out.println("   Sum of all prime numbers upto " + n + " : " + sum);
+    }
+
+}
