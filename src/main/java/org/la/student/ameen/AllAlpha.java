d9264fded954e542c40ddf13d0674257ffb40699
DATA <DATA@DATA-PC.domain.name>
2020-12-19 13:39:51 +0530

    All alphabet from a to z using while loop

---
+++ src/main/java/org/la/student/ameen/AllAlpha.java
@@ -0,0 +1,16 @@
+package org.la.student.ameen;
+
+public class AllAlpha {
+
+	public static void main(String[] args) {
+		//All alphabet from a to z using while loop
+		char c ='a';
+		while(c<='z')
+		{
+			System.out.println("c : " +c);
+			c++;
+		}
+
+	}
+
+}
