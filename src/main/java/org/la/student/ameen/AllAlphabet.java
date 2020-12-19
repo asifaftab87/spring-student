1454e3368e04b290a8e0873eb534118367245003
DATA <DATA@DATA-PC.domain.name>
2020-12-19 13:44:42 +0530

    Print all alphabet from a to z.

---
+++ src/main/java/org/la/student/ameen/AllAlphabet.java
@@ -0,0 +1,19 @@
+package org.la.student.ameen;
+
+public class AllAlphabet {
+
+	public static void main(String[] args) {
+		// Print all alphabet from a to z.
+		char i ='a';
+		for(char c =i;c<='z';c++)
+		{
+		System.out.println("c : " +c);
+		}
+		
+		//for(char c ='a'; c<='z';c++)
+		//{
+		//	System.out.println("c : " +c);
+		//}
+	}
+
+}
