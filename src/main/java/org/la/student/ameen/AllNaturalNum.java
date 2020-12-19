1426b1c29575382e6865930f413303f78f864672
DATA <DATA@DATA-PC.domain.name>
2020-12-19 13:46:42 +0530

    Natural number from 1 to n.(using for loop)

---
+++ src/main/java/org/la/student/ameen/AllNaturalNum.java
@@ -0,0 +1,16 @@
+package org.la.student.ameen;
+
+public class AllNaturalNum {
+
+	public static void main(String[] args) {
+		// Natural number from 1 to n.(using for loop)
+		int num = 1;
+		int n = 8;
+		for(int i = num; i<=n;i++)
+		{
+			System.out.println("i : " +i);
+		}
+
+	}
+
+}
