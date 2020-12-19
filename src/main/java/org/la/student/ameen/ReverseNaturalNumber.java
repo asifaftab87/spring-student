1739c22aef1cbd649544dd3d1404821b6a0e9a60
DATA <DATA@DATA-PC.domain.name>
2020-12-19 13:51:41 +0530

    Reverse Natural Number from n to 1.

---
+++ src/main/java/org/la/student/ameen/ReverseNaturalNumber.java
@@ -0,0 +1,16 @@
+package org.la.student.ameen;
+
+public class ReverseNaturalNumber {
+
+	public static void main(String[] args) {
+		// Reverse Natural Number from n to 1.
+		int n = 8;
+		while(n>=1)
+		{
+			System.out.println("n : " +n);
+			n--;
+		}
+
+	}
+
+}
