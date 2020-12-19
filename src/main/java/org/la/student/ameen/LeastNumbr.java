9931eb5e0cb966bca46b7d2bf8d73a98dbfa3830
DATA <DATA@DATA-PC.domain.name>
2020-12-19 13:41:41 +0530

    Least number among two number

---
+++ src/main/java/org/la/student/ameen/LeastNumbr.java
@@ -0,0 +1,19 @@
+package org.la.student.ameen;
+
+public class LeastNumbr {
+
+	public static void main(String[] args) {
+		// Least number among two number
+		int num1 = 13 , num2=154;
+		if(num1>num2)
+		{
+			System.out.println("num1 is the greatest");
+		}
+		else
+		{
+			System.out.println("num2 is the greatest number");
+		}
+
+	}
+
+}
