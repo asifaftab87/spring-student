3714d23c8579f1a0e88889b76d4a55b2852677c3
DATA <DATA@DATA-PC.domain.name>
2020-12-19 13:52:09 +0530

    Reverse Number from n to 1 (using for loop)

---
+++ src/main/java/org/la/student/ameen/ReverseNum.java
@@ -0,0 +1,15 @@
+package org.la.student.ameen;
+
+public class ReverseNum {
+
+	public static void main(String[] args) {
+		// Reverse Number from n to 1 (using for loop)
+		int num = 8;
+		for(int i = num; i>=1;i--)
+		{
+			System.out.println("i : " +i);
+		}
+
+	}
+
+}
