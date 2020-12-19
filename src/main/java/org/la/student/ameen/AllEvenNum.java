d2c3e11a6cc6c23a062193c3e9387eb6e963fe79
DATA <DATA@DATA-PC.domain.name>
2020-12-19 13:45:26 +0530

    Print all even number from 1 to 100

---
+++ src/main/java/org/la/student/ameen/AllEvenNum.java
@@ -0,0 +1,19 @@
+package org.la.student.ameen;
+
+public class AllEvenNum {
+
+	public static void main(String[] args) {
+		// Print all even number from 1 to 100
+		int num =1;
+		for(int i = num ; i<=100;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println(" i : " +i);
+			}
+			
+		}
+
+	}
+
+}
