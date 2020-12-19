d66a549a22981c141fe88332b1930801a757c350
DATA <DATA@DATA-PC.domain.name>
2020-12-19 17:37:27 +0530

    Wap to print all Prime numbers between 1 to n.

---
+++ src/main/java/org/la/student/ameen/PrimeNumberFrom1ton.java
@@ -0,0 +1,19 @@
+package org.la.student.ameen;
+
+public class PrimeNumberFrom1ton {
+
+	public static void main(String[] args) {
+		// Wap to print all Prime numbers between 1 to n.
+		int n = 5;
+		int num=1;
+		for(int i=num;num<=n;num++)
+		{
+			if(num%2!=0)
+			{
+				System.out.println("num : " +num);
+			}
+		}
+
+	}
+
+}
