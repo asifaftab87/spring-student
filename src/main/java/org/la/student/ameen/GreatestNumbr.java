40e93f3a6c3b49c4972df461798722a505103c93
DATA <DATA@DATA-PC.domain.name>
2020-12-19 13:49:33 +0530

    Greatest number among three

---
+++ src/main/java/org/la/student/ameen/GreatestNumbr.java
@@ -0,0 +1,23 @@
+package org.la.student.ameen;
+
+public class GreatestNumbr {
+
+	public static void main(String[] args) {
+		// Greatest number among three
+		int num1 = 34,num2=154,num3=67;
+		if(num1>num2&&num1>num3)
+		{
+			System.out.println("num1 is the greatest number");
+		}
+		else if(num2>num1&&num2>num3) 
+		{
+			System.out.println("num2 is the greatest number");
+		}
+		else
+		{
+			System.out.println("num3 is the greatest number");
+		}
+
+	}
+
+}
