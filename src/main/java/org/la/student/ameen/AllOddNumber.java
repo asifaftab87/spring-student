994f52f04a7544b727038e3718a214016a077bfb
DATA <DATA@DATA-PC.domain.name>
2020-12-19 13:47:41 +0530

    All odd number from 1 to 100 using for loop

---
+++ src/main/java/org/la/student/ameen/AllOddNumber.java
@@ -0,0 +1,18 @@
+package org.la.student.ameen;
+
+public class AllOddNumber {
+
+	public static void main(String[] args) {
+		// All odd number from 1 to 100 using for loop
+		int num = 1;
+		for(int i =num ; i<=100;i++)
+		{
+			if(i%2!=0)
+			{
+				System.out.println("i : " +i);
+			}
+		}
+
+	}
+
+}
