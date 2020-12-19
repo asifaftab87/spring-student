3e90f82e5c59b99c6c5f3c529750c9785b2f51f9
DATA <DATA@DATA-PC.domain.name>
2020-12-19 13:51:09 +0530

    Print all Natural Number from 1 to n.

---
+++ src/main/java/org/la/student/ameen/NaturalNumber.java
@@ -0,0 +1,17 @@
+package org.la.student.ameen;
+
+public class NaturalNumber {
+
+	public static void main(String[] args) {
+		// Print all Natural Number from 1 to n.
+		int num =1;
+		int n = 5;
+		while(num<=n)
+		{
+			System.out.println("num : " +num);
+			num++;
+		}
+
+	}
+
+}
