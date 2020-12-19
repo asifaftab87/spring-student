b88d9267bec7392c9884aa707cfb30d866b9e57f
DATA <DATA@DATA-PC.domain.name>
2020-12-19 13:42:48 +0530

    count number of digit in a number

---
+++ src/main/java/org/la/student/ameen/CountDigitNum.java
@@ -0,0 +1,19 @@
+package org.la.student.ameen;
+
+public class CountDigitNum {
+
+	public static void main(String[] args) {
+		// count number of digit in a number
+		int num = 12345;
+		int count =0;
+		while(num!=0) 
+		{
+			
+			num=num/10;
+			count++;
+			
+		}
+		System.out.println("count : " +count);
+	}
+
+}
