9285fadf6b79b20a2c62f06026901bfd206665ef
sjdali819 <37649963+sjdali819@users.noreply.github.com>
2020-12-23 05:13:55 +0530

    by sajid

---
+++ src/main/java/org/la/student/sajid/DiamondStarPrint.java
@@ -0,0 +1,34 @@
+package org.la.student.sajid;
+
+public class DiamondStarPrint {
+	public static void main(String[] args) {
+//		Diamond Pattern
+		
+		int i,j,r=5;
+		
+		 for(i=0;i<=r;i++)
+		   {
+		     for(j=1;j<=r-i;j++) {
+		    	 System.out.print(" ");
+		     }
+		     for(j=1;j<=i-1;j++) {
+		    	 System.out.print("* ");
+		     }
+		     
+		     	System.out.println("");
+		   }
+		 
+		   for(i=r-1;i>=1;i--)
+		   {
+			   
+		     for(j=1;j<=r-i;j++) {
+		    	 System.out.print(" ");
+		     }
+		     for(j=1;j<=i-1;j++) {
+		    	 System.out.print("* ");
+		     }
+		     	System.out.println("");
+		   }
+	}
+
+}
