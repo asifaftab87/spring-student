a4f199ee4375af7cb49ad10a0541b0a2804e14d0
Aameen <Aameen@DATA-PC.domain.name>
2020-12-25 00:35:23 +0530

    wap to print number from 50 to 45

---
+++ src/main/java/org/la/student/ameen/PrintNumberReverse.java
@@ -0,0 +1,15 @@
+package org.la.student.ameen;
+
+public class PrintNumberReverse {
+
+	public static void main(String[] args) {
+		// wap to print number from 50 to 45
+		int num = 50;
+		for(int i = num ; i>=45;i--)
+		{
+			System.out.println(" i : " +i);
+		}
+
+	}
+
+}
