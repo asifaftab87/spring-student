0c3a208c8f988ced9b8163c2c6d05a08dd9e0cc9
Aameen <Aameen@DATA-PC.domain.name>
2020-12-24 23:59:04 +0530

    Wap to check whether a number is Armstrong number or not

--- src/main/java/org/la/student/ameen/ArmstrongNumber.java
+++ src/main/java/org/la/student/ameen/ArmstrongNumber.java
@@ -3,8 +3,28 @@ package org.la.student.ameen;
 public class ArmstrongNumber {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Wap to check whether a number is Armstrong number or not
+		 int c=0;
+				 int a;  
+		    int n=153;
+		   int Armstrong=n;  
+		    while(n>0)  
+		    {  
+		    a = n % 10;  
+		    n = n / 10;  
+		    c = c+(a*a*a);  
+		    }  
+		    if(Armstrong==c) 
+		    {
+		    System.out.println("armstrong number");   
+		    }
+		    else  
+		    {
+		    	System.out.println("Not armstrong number");   
+		    }
+		    
+		    }  
+		 
 	}
 
-}
+
