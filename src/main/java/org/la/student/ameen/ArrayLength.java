765809760d92a94fc4bc1f4aa8abb2abef0cd87f
Aameen <Aameen@DATA-PC.domain.name>
2020-12-26 12:28:27 +0530

    wap to find length of an array

---
+++ src/main/java/org/la/student/ameen/ArrayLength.java
@@ -0,0 +1,12 @@
+package org.la.student.ameen;
+
+public class ArrayLength {
+
+	public static void main(String[] args) {
+		//wap to find length of an array.
+		int array [] = new int[5];
+		System.out.println(" the length of an array : " +array.length);
+
+	}
+
+}
