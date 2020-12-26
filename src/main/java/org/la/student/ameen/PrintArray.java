ce6a6b1235448b433209217b9bea541090337029
Aameen <Aameen@DATA-PC.domain.name>
2020-12-26 12:10:12 +0530

    wap to create and print array using without loop

---
+++ src/main/java/org/la/student/ameen/PrintArray.java
@@ -0,0 +1,22 @@
+package org.la.student.ameen;
+
+public class PrintArray {
+
+	public static void main(String[] args) {
+		// 01) wap to create and print array using without loop
+		int a []  = new int [5];
+		a[0]=42;
+		a[1]=289;
+		a[2]=23;
+		a[3]=21;
+		a[4]=587;
+	
+		
+		System.out.println(a[0]);
+		System.out.println(a[1]);
+		System.out.println(a[2]);
+		System.out.println(a[3]);
+		System.out.println(a[4]);
+	}
+
+}
