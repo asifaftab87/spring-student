0c02d811ae64ab110295a6fa55ca5d1e1e1c0284
nourahamdi <73748252+nourahamdi@users.noreply.github.com>
2020-12-28 23:34:33 +0300

    noura

---
+++ src/main/java/org/la/student/noura/array/Add2ArrayElementsandCopyintoThirdArray.java
@@ -0,0 +1,23 @@
+package org.la.student.noura.array;
+
+public class Add2ArrayElementsandCopyintoThirdArray {
+
+	public static void main(String[] args) {
+		//wap to add two array elements n copy into third array
+	 
+		int a[]= {1,2,3,4,5};
+		int b[]= {5,4,6,7,9};
+		int c[]= new int [a.length];
+		
+		for (int i=0;i<b.length;i++) {
+			
+			c[i]=a[i]+b[i];
+			
+			System.out.print(" "+c[i]);
+
+			
+		}
+		
+	}
+
+}
