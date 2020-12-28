39721cb0477101ceef548f9954c97ec5620c283f
Nawaz <Lenovo@LAPTOP-9DV8BD27>
2020-12-28 21:59:38 +0530

    Star Pattern Increasing Order

---
+++ src/main/java/org/la/student/nawaz/PatternPrintStar.java
@@ -0,0 +1,17 @@
+package org.la.student.nawaz;
+
+public class PatternPrintStar {
+	
+	public static void main(String[] args) {
+		
+		for (int i=1 ; i<=8 ; i++){
+			
+			for (int k=1 ; k<=i ; k++){
+				
+				System.out.print(" * ");
+			} 
+			System.out.println(" ");
+		}
+	}
+
+}
