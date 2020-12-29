4b723b99f67fb3723f1c83d1412f79501d00287e
Nawaz <Lenovo@LAPTOP-9DV8BD27>
2020-12-29 12:56:27 +0530

    Numbers Print

---
+++ src/main/java/org/la/student/nawaz/NumbersPrint.java
@@ -0,0 +1,14 @@
+package org.la.student.nawaz;
+
+public class NumbersPrint {
+	
+	public static void main(String[] args) {
+		
+		for (int i=0; i<=9;i++) {
+			
+			System.out.println(i);
+		}
+		
+		}
+	
+     }
