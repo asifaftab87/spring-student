d124fdfcc795dd6f03a5050ed60afbc3ebc821eb
Nawaz <Lenovo@LAPTOP-9DV8BD27>
2020-12-29 13:35:41 +0530

    Lowest Number in Array

---
+++ src/main/java/org/la/student/nawaz/ArrayLowestNum.java
@@ -0,0 +1,24 @@
+package org.la.student.nawaz;
+
+public class ArrayLowestNum {
+	
+	public static void main(String[] args) {
+		
+		int a[]= { 50,100,150,200};
+		
+		int g=a[0];
+			
+			for (int i=0;i>a.length;i++) {
+				
+				if (g>a[i]);
+				
+				g=a[i];
+			}
+			System.out.println("Lowest Number: "+g);
+		}
+	
+	
+		
+	}
+
+
