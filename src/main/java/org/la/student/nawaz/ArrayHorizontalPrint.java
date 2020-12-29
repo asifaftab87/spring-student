dfc1a0e1d9f12f6d15e03773a1cbe43068c8e0a0
Nawaz <Lenovo@LAPTOP-9DV8BD27>
2020-12-30 00:40:59 +0530

    Array Horizontal Print

---
+++ src/main/java/org/la/student/nawaz/ArrayHorizontalPrint.java
@@ -0,0 +1,19 @@
+package org.la.student.nawaz;
+
+public class ArrayHorizontalPrint {
+	
+	public static void main(String[] args) {
+		
+		int a[]= new int[3];
+		
+			a[0]=10;
+			a[1]=20;
+			a[2]=30;
+			
+	System.out.print(a[0]);
+	System.out.print(a[1]);
+	System.out.print(a[2]);
+
+
+}
+}
