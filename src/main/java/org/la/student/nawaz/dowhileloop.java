d8ef1a89e2d3310d978bac9e8f9da17865664f82
Nawaz <Lenovo@LAPTOP-9DV8BD27>
2020-12-31 00:39:38 +0530

    Example do while Loop

---
+++ src/main/java/org/la/student/nawaz/dowhileloop.java
@@ -0,0 +1,18 @@
+package org.la.student.nawaz;
+
+public class dowhileloop {
+	
+	public static void main(String[] args) {
+		
+		int i=1;
+		do
+		{
+			System.out.println(i);
+			i++;
+		}
+		while (i<=11);
+			
+			
+	}
+
+}
