49700aa002930c5ce1970d2642db26541652ffd9
nourahamdi <73748252+nourahamdi@users.noreply.github.com>
2021-01-01 23:05:17 +0300

    noura

--- src/main/java/org/la/student/noura/array/CountOccurrencesofEachElementinArray.java
+++ src/main/java/org/la/student/noura/array/CountOccurrencesofEachElementinArray.java
@@ -11,16 +11,17 @@ public class CountOccurrencesofEachElementinArray {
         int [] fr = new int [arr.length];  
        
         int visited = -1;  
-       
+        
         for(int i = 0; i < arr.length; i++){  
             int count =1; 
-;  
+
             for(int j = i+1; j < arr.length; j++){  
                 if(arr[i] == arr[j]){  
                     count++;  
                     fr[j] = visited; //To avoid counting same element again  
+                    }
 
-                                    }  
+                                 
             }  
             if(fr[i] != visited)  
                 fr[i] = count;  
