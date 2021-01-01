9fe43cb6d91bcf2fb40d58eb3ff2d7dd4a99e256
Nawaz <Lenovo@LAPTOP-9DV8BD27>
2021-01-01 23:42:42 +0530

    One more example of For Loop

---
+++ src/main/java/org/la/student/nawaz/ExampleForLoop1.java
@@ -0,0 +1,13 @@
+package org.la.student.nawaz;
+
+public class ExampleForLoop1 {
+	
+	public static void main(String[] args) {
+		
+		for (int i=1;i<=10;i++) {
+			
+			System.out.println(i++);
+		}
+	}
+
+}
