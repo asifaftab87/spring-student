4da0376925c4f2dd3043face16a7aea7f7c92e26
Nawaz <Lenovo@LAPTOP-9DV8BD27>
2021-01-06 00:53:47 +0530

    Example Methods 2

---
+++ src/main/java/org/la/student/nawaz/Methods2.java
@@ -0,0 +1,81 @@
+package org.la.student.nawaz;
+
+public class Methods2 {
+	
+	public static void main(String[] args) {
+		
+		Season1();
+		Season2();
+		Season3();
+		Season4();
+		Season5();
+		Season6();
+		Season7();
+		Season8();
+		Season9();
+		Season10();
+		Season11();
+		Season12();
+		Season13();
+		Season14();
+		
+	}
+		
+		public static void Season1(){
+			System.out.println("Welcome to IPL 2007");
+		}
+		
+		public static void Season2() {
+			System.out.println("Welcome to IPL 2008");
+		}
+		
+		public static void Season3(){
+			System.out.println("Welcome to IPL 2009");
+		}
+		
+		public static void Season4() {
+			System.out.println("Welcome to IPL 2010");
+		}
+		
+		public static void Season5(){
+			System.out.println("Welcome to IPL 2011");
+		}
+		
+		public static void Season6() {
+			System.out.println("Welcome to IPL 2012");
+		}
+		
+		public static void Season7(){
+			System.out.println("Welcome to IPL 2013");
+		}
+		
+		public static void Season8() {
+			System.out.println("Welcome to IPL 2014");
+		}
+		
+		public static void Season9(){
+			System.out.println("Welcome to IPL 2015");
+		}
+		
+		public static void Season10() {
+			System.out.println("Welcome to IPL 2016");
+		}
+		
+		public static void Season11(){
+			System.out.println("Welcome to IPL 2017");
+		}
+		
+		public static void Season12() {
+			System.out.println("Welcome to IPL 2018");
+		}
+		
+		public static void Season13(){
+			System.out.println("Welcome to IPL 2019");
+		}
+		
+		public static void Season14() {
+			System.out.println("Welcome to IPL 2020");
+		}
+	}
+
+
