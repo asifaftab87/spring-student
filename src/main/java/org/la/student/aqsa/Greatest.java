4bda994e27f2c27b1b571cde40ec23b2eb5cb1a7
aqsa <nd@Anonymous>
2021-01-06 13:50:33 +0530

    Greatest no from an array

---
+++ src/main/java/org/la/student/aqsa/Greatest.java
@@ -0,0 +1,23 @@
+package org.la.student.aqsa;
+import java.util.*;
+public class Greatest {
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int[] a= new int[10];
+		for(int i=0;i<a.length;i++) {
+			a[i]=sc.nextInt();
+		                            }
+		sc.close();
+		System.out.print(greatestElement(a));
+		                                       }
+		static int greatestElement(int[] a) {
+			int greatest=a[0];
+			for(int i=1;i<a.length;i++) {
+				if(greatest<a[i]) {
+					greatest=a[i];
+				}
+			}
+			return greatest;
+		}
+
+}
