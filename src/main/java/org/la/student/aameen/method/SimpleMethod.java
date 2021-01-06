01c41d3d155746c5ce8a33343424dbc9981903f4
Aameen <Aameen@DATA-PC.domain.name>
2021-01-06 15:41:59 +0530

    Method Accept argument but not returning

---
+++ src/main/java/org/la/student/aameen/method/SimpleMethod.java
@@ -0,0 +1,16 @@
+package org.la.student.aameen.method;
+
+public class SimpleMethod {
+
+	public static void main(String[] args) {
+		// Method Accept argument but not returning
+		System.out.println(" Main Method");
+		
+		accept(7);
+	}
+	static void accept(int a )
+	{
+		System.out.println(" the value of argument , a  : " +a);
+	}
+
+}
