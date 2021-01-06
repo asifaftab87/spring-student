e9c2dc4676da25501b60943eb75ff0621813fe87
aqsa <nd@Anonymous>
2021-01-06 14:47:13 +0530

    print the prime numbers in an array

--- src/main/java/org/la/student/aqsa/PrimeArray.java
+++ src/main/java/org/la/student/aqsa/PrimeArray.java
@@ -1,8 +1,7 @@
 package org.la.student.aqsa;
 
 import java.util.Scanner;
-
-public class Smallest {
+public class PrimeArray {
 	public static void main(String[] args) {
 		Scanner sc= new Scanner(System.in);
 		int[] a= new int[10];
@@ -10,16 +9,19 @@ public class Smallest {
 			a[i]=sc.nextInt();
 			}
 		sc.close();
-		System.out.print(smallestElement(a));
-		}
-		static int smallestElement(int[] a) {
-			int smallest=a[0];
-			for(int i=1;i<a.length;i++) {
-				if(smallest>a[i]) {
-					smallest=a[i];
+		primeno(a);
+}
+	static void primeno(int[] a) {
+		for(int i=0;i<10;i++) {
+			int k=0;
+			for(int j=1;j<=a[i];j++) {
+				if(a[i]%j==0) {
+				k++	;
 				}
 			}
-			return smallest;
+		if(k==2) {
+			System.out.println(a[i]);
 		}
-
+		}
+	}
 }
