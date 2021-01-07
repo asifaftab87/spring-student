eb1bf5e0815cbb92a817c790b16c1ac498e2073b
Nawaz <Lenovo@LAPTOP-9DV8BD27>
2021-01-07 13:46:12 +0530

    Example of Methods3

---
+++ src/main/java/org/la/student/nawaz/Methods3.java
@@ -0,0 +1,38 @@
+package org.la.student.nawaz;
+
+public class Methods3 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("My Methods Program Practicing");
+		System.out.println();
+		
+		ThirdMethod();
+		SecondMethod();
+		FirstMethod();
+		
+		System.out.println();
+		System.out.println("My Methods Program Practicing");
+		System.out.println();
+		
+		FirstMethod();
+		SecondMethod();
+		ThirdMethod();
+	}
+	
+	static void FirstMethod() {
+		
+		System.out.println("Welcome to First Method");
+	}
+	
+	static void SecondMethod() {
+		
+		System.out.println("Welcome to Second Method");
+	}
+	
+	static void ThirdMethod() {
+		
+		System.out.println("Welcome to Third Method");
+	}
+
+}
