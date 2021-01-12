172f9d97d14c495fb9c88c7c2186f07ffa393a58
Nawaz <Lenovo@LAPTOP-9DV8BD27>
2021-01-12 16:04:14 +0530

    Even Or Odd Number Check1

---
+++ src/main/java/org/la/student/nawaz/EvenOrOdd.java
@@ -0,0 +1,19 @@
+package org.la.student.nawaz;
+
+public class EvenOrOdd {
+	
+	public static void main(String[] args) {
+		
+		int n=75;
+		
+		if (n%2==0) {
+			
+			System.out.println("The Number is Even");
+		}
+		else {
+			
+			System.out.println("The Number is Odd");
+		}
+	}
+
+}
