6a0f5611f56a66390541c70ff7bd9bf51f669a9c
Nawaz <Lenovo@LAPTOP-9DV8BD27>
2021-01-20 01:11:07 +0530

    Upper Case Alphabets Print

---
+++ src/main/java/org/la/student/nawaz/UpperCaseCharactersPrint.java
@@ -0,0 +1,18 @@
+package org.la.student.nawaz;
+
+public class UpperCaseCharactersPrint {
+	
+	public static void main(String[] args) 
+	
+	{
+		char c;
+	
+	    for (c='A'; c<='Z'; ++c)
+		
+	    {
+	    	System.out.print(c + " ");
+	    }
+
+}
+
+}
