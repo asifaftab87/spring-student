9e7ceeb5986f562b3a95a67367c051f4b1889c67
Nawaz <Lenovo@LAPTOP-9DV8BD27>
2021-01-27 00:12:24 +0530

    Second Practice on using different Classes

---
+++ src/main/java/org/la/student/nawaz/classes/Animal.java
@@ -0,0 +1,11 @@
+package org.la.student.nawaz.classes;
+
+public class Animal {
+	
+	public static void main(String[] args) {
+		
+		Rabbit RB= new Rabbit();
+		RB.PrintSomething();
+	}
+
+}
---
+++ src/main/java/org/la/student/nawaz/classes/Rabbit.java
@@ -0,0 +1,10 @@
+package org.la.student.nawaz.classes;
+
+public class Rabbit {
+	
+	public void PrintSomething() {
+		
+		System.out.println("This is from the Rabbit Class");
+	}
+
+}
