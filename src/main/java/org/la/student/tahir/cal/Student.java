9ced3e1db7aeff7d0c7020768dd95bfd17309f57
Dalori <Dalori@10.211.55.8>
2021-02-15 15:25:30 +0800

    Assignment Update

---
+++ src/main/java/org/la/student/tahir/cal/Student.java
@@ -0,0 +1,32 @@
+package org.la.student.tahir.cal;
+
+public class Student {
+
+	private int id;
+	private String name;
+	
+	
+	
+	public void neew() {
+		
+		System.out.println("From call package");
+		
+		
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id= id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name= name;
+	}
+}
---
+++ src/main/java/org/la/student/tahir/clas/DiffPackageConcept.java
@@ -0,0 +1,23 @@
+package org.la.student.tahir.clas;
+
+import org.la.student.tahir.cal.Student;
+
+
+public class DiffPackageConcept {
+
+	public static void main(String[] args) {
+	
+		
+		Student student = new Student();
+		
+		student.neew();
+		
+		student.setId(20);
+		student.setName("Zainab");
+		
+		System.out.println(student.getId());
+		System.out.println(student.getName());
+
+	}
+
+}
