f5243411027e625edbfc228b84b1645ca360727c
aqsa <nd@Anonymous>
2021-02-21 15:23:48 +0530

    update

--- src/main/java/org/la/student/aqsa/B1.java
+++ src/main/java/org/la/student/aqsa/B1.java
@@ -1,9 +1,29 @@
 package org.la.student.aqsa;
+import org.la.student.aqsa.Protected.Prtct;
 
-public class B1 {
+	public class B1 extends Prtct {
+public B1() {
+	super.stand();
+	
+}
+		 public static void main(String[] args) {
+				B1 b  = new B1();
+				b.stand();
+				System.out.println(b.i);
+				
+			//	Prtct p = new Prtct(); parent class constructor cant be inherited to child why???//
+			//	Prtct p = new Prtct();
+				Prtct p1 = new B1();
+				p1.stand();
+				System.out.println(p1.i);
+			   
+		 }
+		 
+		 @Override
+		 public void stand() {
+				System.out.println(" Child method Stand");
+				
+			}
 
-	public B1() {
-		// TODO Auto-generated constructor stub
 	}
 
-}
--- src/main/java/org/la/student/aqsa/Protected/Prtct.java
+++ src/main/java/org/la/student/aqsa/Protected/Prtct.java
@@ -2,8 +2,21 @@ package org.la.student.aqsa.Protected;
 
 public class Prtct {
 
-	public Prtct() {
-		// TODO Auto-generated constructor stub
+		public int i;
+	  
+		protected Prtct(){
+		this(5);
+	   }
+	   protected Prtct(int i) {
+		   this.i=i;
+		   System.out.println("parametarised "+i);   
+	    } 
+	   
+	   public void stand() {
+			// TODO Auto-generated method stub
+			System.out.println(" Parent method Stand");
+			
+		}
+
 	}
 
-}
