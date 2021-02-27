5c8320e9b8888270aa0ed80d8cd42de3ac7104e0
Nawaz <Lenovo@LAPTOP-9DV8BD27>
2021-02-27 16:38:35 +0530

    Programs of Inheritance and Super Keyword

---
+++ src/main/java/org/la/student/nawaz/Inhrtnce/MainInheritance.java
@@ -0,0 +1,18 @@
+package org.la.student.nawaz.Inhrtnce;
+
+public class MainInheritance {
+	
+
+	  public static void main(String[] args) {
+
+	    Dog labrador = new Dog();
+
+	    
+	    labrador.eat();
+	    labrador.bark();
+	  }
+	}
+
+
+
+
---
+++ src/main/java/org/la/student/nawaz/Inhrtnce/ProgramInheritance.java
@@ -0,0 +1,28 @@
+package org.la.student.nawaz.Inhrtnce;
+
+public class ProgramInheritance {
+	
+	public void eat() {
+	    System.out.println("The Animal like to eat Grass");
+	  }
+	}
+
+
+	class Dog extends Animal1 {
+
+	  
+	  @Override
+	  public void eat() {
+
+	   
+	    super.eat();
+	    System.out.println("The Dog like to eat food");
+	  }
+
+	  
+	  public void bark() {
+	    System.out.println("The Dog bark");
+	  }
+	}
+
+
