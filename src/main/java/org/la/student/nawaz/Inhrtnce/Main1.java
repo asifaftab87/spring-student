da22a003caef9649d01741aeaa54dabcd43959fe
Nawaz <Lenovo@LAPTOP-9DV8BD27>
2021-02-27 16:13:20 +0530

    Examples using Inheritance , Super keyword

---
+++ src/main/java/org/la/student/nawaz/Inhrtnce/Main1.java
@@ -0,0 +1,16 @@
+package org.la.student.nawaz.Inhrtnce;
+
+public class Main1 {
+	
+	
+		  public static void main(String[] args) {
+
+		    Dog labrador = new Dog();
+
+		    
+		    labrador.eat();
+		    labrador.bark();
+		  }
+		}
+
+
