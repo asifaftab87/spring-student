d7c063b3c9198a92a312cf0a6b020e504170869b
Abdullah Jamal <Abdullah Jamal@192.168.1.15>
2021-04-04 19:41:10 -0500

    This is Jamal and i am testing how i can push folder

---
+++ src/main/java/org/la/student/jamal/FirstClass.java
@@ -0,0 +1,24 @@
+package org.la.student.jamal;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Welcome to Java program ! \n This is Jamal ! \n Welcame to my world :)");
+		
+		String name = "Jamal";
+		String live = "Peru";
+		String learning ="java";
+		
+		
+	   System.out.println("My name is " + name +" I live in "+ live +" I am learning "+ learning  );
+	   
+		
+		
+		
+		
+
+	}
+
+}
