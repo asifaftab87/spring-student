8bb9fd28757dd11ebf094559ebc72502357c0bd2
Tania32855 <HP@LAPTOP-MDJKCEIE>
2021-04-09 14:18:12 +0800

    first_class_prog

---
+++ src/main/java/org/la/student/tania/First_class_prog.java
@@ -0,0 +1,51 @@
+package org.la.student.tania;
+
+public class First_class_prog {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Wap to print 3 times hello world
+		
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		
+		//Wap to decalre an int variable n print
+		int a=5;
+		System.out .println("a " +a);
+		
+		//Wap to decalre a char variable n print it
+		char b ='A';
+		System.out.println(b);
+		
+		//Wap to decalre a double variable n print it
+		double x=6352.54;
+		System.out.println("x " +x);
+		
+		//Wap to decalre a float variable n print it
+		float y= 2.5f;
+		System.out.println("y " +y);
+		
+		//Wap to decalre a boolean variable n print it
+		boolean z=true; 
+		System.out.println("z " +z);
+		
+		//Wap to print java world
+		
+		System.out.println("java world");
+		
+		// Wap to print java classes started 
+		
+		System.out.println("java class started");
+		
+		//Wap to print world hello 5 times
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		
+	}
+
+}
