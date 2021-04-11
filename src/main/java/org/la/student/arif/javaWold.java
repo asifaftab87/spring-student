c0c85fc87a5e458267f68f967028d69cd38d0b4d
arifr <arifr@DESKTOP-ARIF>
2021-04-11 16:57:24 +0800

    Wap to print 3 times hello world
    Wap to decalre an int variable n print
    
    Wap to decalre a char variable n print it
    
    Wap to decalre a double variable n print it
    
    Wap to decalre a float variable n print it
    
    Wap to decalre a boolean variable n print it
    
    Wap to print java world
    
    Wap to print java classes started
    
    Wap to print world hello 5 times
    
    Task Completed

---
+++ src/main/java/org/la/student/arif/javaWold.java
@@ -0,0 +1,11 @@
+package org.la.student.arif;
+
+public class javaWold {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Java World");
+
+	}
+
+}
