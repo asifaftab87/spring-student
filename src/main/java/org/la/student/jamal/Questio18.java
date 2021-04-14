b26495ae1b1caf3f39dd45bac529e014047f7e15
Abdullah Jamal <Abdullah Jamal@192.168.1.22>
2021-04-14 12:35:46 -0500

    second class assignment

---
+++ src/main/java/org/la/student/jamal/Questio18.java
@@ -0,0 +1,18 @@
+package org.la.student.jamal;
+
+public class Questio18 {
+
+	public static void main(String[] args) {
+		
+		int num = 69;
+        int days = num % 30;
+        int month = num / 30;
+        System.out.println(month + " Month and " + days + " days");
+        
+       
+        	
+
+
+	}
+
+}
---
+++ src/main/java/org/la/student/jamal/Question15.java
@@ -0,0 +1,26 @@
+package org.la.student.jamal;
+
+public class Question15 {
+
+	public static void main(String[] args) {
+		
+		int i = 34;
+        int j = 7;
+        int k = i % j;
+        
+        //here we have 2 options for (i) to work 1- to take the .0 out because it is int or we can change the int to double .
+        
+        // i will do all the options 
+        
+        System.out.println("These is the answer of question number 15:  k = " + k );
+        
+        
+        double a= 34.0;
+        int b= 7;
+        double c = a % b; 
+        System.out.println("These is the answer of question number 15:  c = " + c );
+
+
+	}
+
+}
