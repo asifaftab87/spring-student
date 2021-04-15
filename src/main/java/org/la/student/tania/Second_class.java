f21d9f8a07894a9f6bd9738265da5b3d9730f2bf
Tania32855 <HP@LAPTOP-MDJKCEIE>
2021-04-15 23:47:33 +0800

    second_class

---
+++ src/main/java/org/la/student/tania/Second_class.java
@@ -0,0 +1,72 @@
+package org.la.student.tania;
+
+public class Second_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//wap to create and print an int variable?
+		int n;
+		n=20;
+		System.out.println("n=" +n);
+		
+		//wap to create and print a char variable?
+		
+		char x= 'A';
+		System.out.println("x=" +x);
+		
+		//wap to create and print a float variable?
+		
+		float b= 10.25f;
+		System.out.println("b=" +b);
+		
+		//wap to create and print a short variable?
+		
+		short number=1456;
+		System.out.println("number=" +number);
+		
+		//wap to create and print a boolean variable?
+		
+		boolean c= true;
+		System.out.println("c=" +c);
+		
+		//wap to create and print a double variable?
+		double num = 1254612.3;
+		System.out.println("num=" +num);
+		
+		//wap to create and print a long variable?
+		
+		long y=5556666L;
+		System.out.println("y=" +y);
+		
+		//wap to create a char variable but assign an int value and print that variable?
+		
+		char ch = 84;
+		System.out.println("ch=" +ch);
+		
+		//wap to create an int variable but assign a char value and print that variable?
+		int m= 't';
+		System.out.println("m=" +m);
+		
+		//1.
+		int i = 19/5;
+		System.out.println("i=" +i);
+		//2.
+		char a = 65;
+		System.out.println("a=" +a);
+		//3.
+		int s = 'a';
+		System.out.println("s=" +s);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
