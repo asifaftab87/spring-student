81816e4d1b171952af6dbbb231d5723833eb6ac1
Md <Lenovo@LAPTOP-9DV8BD27>
2021-04-16 15:11:26 +0530

    Program for Multiplication of 3 Numbers

---
+++ src/main/java/org/la/student/Md/Multiply3Numbers.java
@@ -0,0 +1,17 @@
+package org.la.student.Md;
+
+public class Multiply3Numbers {
+
+	public static void main(String[] args) {
+
+		int w=25;
+		int x=20;
+		int y=10;
+		
+		int z=w*x*y;
+		
+		System.out.println("The Multiplication of 3 Numbers w, x and y : " +z);
+
+	}
+
+}
