71c60c60e8f68a05882203e0c6973be3535773b4
Md <Lenovo@LAPTOP-9DV8BD27>
2021-04-16 14:28:53 +0530

    Simple Output Programs

---
+++ src/main/java/org/la/student/Md/Output.java
@@ -0,0 +1,21 @@
+package org.la.student.Md;
+
+public class Output {
+
+	public static void main(String[] args) {
+		
+		int i=19/5;
+		int k= 19%5;
+		char a =65;
+		int s= 'a';
+		double x = 14/5;
+		
+		System.out.println("The value of i : " +i);
+		System.out.println("The value of k : " +k);
+		System.out.println("The value of a : " +a);
+		System.out.println("The value of s : " +s);
+		System.out.println("The value of x : " +x);
+
+	}
+
+}
