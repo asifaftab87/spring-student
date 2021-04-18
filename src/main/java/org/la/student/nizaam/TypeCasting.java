d03c2d79898d059f971e7c090dda2f725199f958
Nizaam <nizaam.s.m@gmail.com>
2021-04-18 11:17:51 +0530

    ExerciseProgram2, Arithmetic Operations, TypeCasting and Complex Operations

---
+++ src/main/java/org/la/student/nizaam/TypeCasting.java
@@ -0,0 +1,29 @@
+package org.la.student.nizaam;
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//wap to create a char variable but assign an int value and print that variable?
+		char c=65;
+		System.out.println("Char with int value c:"+c);
+		
+		//wap to create an int variable but assign a char value and print that variable?
+		int b='c';
+		System.out.println("Int with Char value i:"+b);
+		
+		//what will be the output?
+				
+	//	1.	
+					int i = 19/5; 
+					System.out.println("value of i : "+i);
+	//	2.  
+					char a = 65;
+					System.out.println("value of a:"+a);
+	//	3.
+					int s = 'a';
+					System.out.println("value of s:"+s);
+	}
+
+}
