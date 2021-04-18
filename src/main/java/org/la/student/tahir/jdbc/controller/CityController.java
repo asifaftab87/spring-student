f2d4609ad39db21e2762c38661225773d3a52287
Tahir Dalori <Dalori@10.211.55.8>
2021-04-18 23:28:55 +0800

    JDBC MVC Program of City table

---
+++ src/main/java/org/la/student/tahir/jdbc/controller/CityController.java
@@ -0,0 +1,65 @@
+package org.la.student.tahir.jdbc.controller;
+
+
+import java.util.List;
+
+
+import org.la.student.tahir.jdbc.model.City;
+import org.la.student.tahir.jdbc.service.CityService;
+
+public class CityController {
+	
+	City city = new City();
+	CityService citService = new CityService();
+
+	public static void main(String[] args) {
+		
+		CityController citControl = new CityController();
+		
+		//citControl.addCity();
+		//citControl.findById(1);
+		citControl.findByCity("Yerwa");
+		
+	}
+
+		
+	
+	void findById(int id) {
+			System.out.println("----------findbyid city---------------");
+			
+			City city = citService.findById(id);
+			if(city==null) {
+				System.out.println("city not found");
+			}
+			else {
+				System.out.println("City: "+city);
+			}
+		}
+		
+		void findByCity(String city) {
+			
+			System.out.println("----------findbyCity city---------------");
+			
+			List<City> cit = citService.findByCity(city);
+			
+			System.out.println(cit);
+		}
+		
+
+		void addCity() {
+
+			System.out.println("----------Adding city---------------");
+			
+			City city = new City();
+			
+			city.setCity("Yerwa");
+			city.setId(2);
+			
+			citService.create(city);
+			
+		}
+		
+
+	
+
+}
