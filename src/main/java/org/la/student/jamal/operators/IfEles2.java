37a556f8150681233da435ccb4d617623bc9f865
Jamal-Ak <Abdullah Jamal@192.168.1.17>
2021-04-24 11:47:09 -0500

    if -else

---
+++ src/main/java/org/la/student/jamal/operators/IfEles2.java
@@ -0,0 +1,33 @@
+package org.la.student.jamal.operators;
+
+public class IfEles2 {
+	/*
+	 * 2.	wap to find which number is greater among three numbers
+	 */
+
+	public static void main(String[] args) {
+		
+		
+		int x=160, y=170, w=180;
+		
+		
+		
+		
+		
+		if  ((x>y) && (x>w)) {
+			System.out.println("greater x  : " + x );
+	
+		}
+		
+		else if ((y>x) && (y>w)) { 
+			System.out.println("greater y  : "+y);
+			
+		}
+		
+		else  {
+			System.out.println("greater w  : " + w);
+		}
+			
+	}
+
+}
