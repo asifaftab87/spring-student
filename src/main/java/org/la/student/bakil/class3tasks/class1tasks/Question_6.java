0f090af890817dd13e7a2b50bc4769ea65ea38a1
Bakil Alkhaledi <alkhaledi.khaled12@gmail.com>
2021-04-28 22:55:31 +0800

    First class data type

---
+++ src/main/java/org/la/student/bakil/class3tasks/class1tasks/Question_6.java
@@ -0,0 +1,15 @@
+package org.la.student.bakil.class3tasks.class1tasks;
+
+public class Question_6 {
+
+	/*
+	 * Wap to print java world 
+	 */
+	public static void main(String[] args) {
+	
+
+		String x="Java world";
+		System.out.println(x);
+	}
+
+}
---
+++ src/main/java/org/la/student/bakil/class3tasks/class1tasks/Question_8.java
@@ -0,0 +1,15 @@
+package org.la.student.bakil.class3tasks.class1tasks;
+
+public class Question_8 {
+
+	/*
+	 * Wap to print world hello 5 times  
+	 */
+	public static void main(String[] args) {
+	
+
+		String x="World hello";
+		System.out.println(x+'\n'+x+'\n'+x+'\n'+x+'\n'+x);
+	}
+
+}
