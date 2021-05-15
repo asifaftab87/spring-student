9260364717e5113468c39f3e33d6b40ccf5550dc
md <Lenovo@LAPTOP-9DV8BD27>
2021-05-15 19:11:22 +0530

    Array in Reverse Order

---
+++ src/main/java/org/la/student/mdnawaz/ArrayReverse.java
@@ -0,0 +1,29 @@
+package org.la.student.mdnawaz;
+
+public class ArrayReverse {
+
+	public static void main(String[] args) {
+
+		 
+        int [] arr = new int [] {1, 2, 3, 4, 5};  
+        
+              System.out.println("Original array: ");  
+              
+                for (int i = 0; i < arr.length; i++)
+                	
+                {  
+                	 System.out.print(arr[i] + " ");  
+                }  
+                   System.out.println(); 
+                   
+              System.out.println("Array in reverse order: ");  
+       
+               for (int i = arr.length-1; i >= 0; i--) 
+               
+               {  
+            	   System.out.print(arr[i] + " ");  
+               }  
+
+	}
+
+}
