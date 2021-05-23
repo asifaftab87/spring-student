a29523fedcb1cd93edee59d54dbc64e8b956c370
Bakil Alkhaledi <alkhaledi.khaled12@gmail.com>
2021-05-23 13:20:22 +0800

    Arrays tasks

--- src/main/java/org/la/student/bakil/class6tasks_arry/ArrayContainsSpecificValue.java
+++ src/main/java/org/la/student/bakil/class6tasks_arry/ArrayContainsSpecificValue.java
@@ -7,9 +7,12 @@ public class ArrayContainsSpecificValue {
 	public static void main(String[] args) {
 	
 		int[] a = {20, 30, 25, 35, 16, 60, 100};
-	       //calculate sum of all array elements
-	       int sum = 0;
-	       for(int i=0; i < a.length ; i++)
+	      
+		//calculate sum of all array elements
+	       
+		int sum = 0;
+	      
+		for(int i=0; i < a.length ; i++)
 	        sum = sum + a[i];
 	       //calculate average value
 	        int average = sum / a.length;
