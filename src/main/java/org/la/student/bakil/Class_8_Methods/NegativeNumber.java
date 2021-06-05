ad70fbc8f9e2f3172c70c3d623aea6759d540699
Bakil Alkhaledi <alkhaledi.khaled12@gmail.com>
2021-06-05 12:41:48 +0800

    Method tasks

--- src/main/java/org/la/student/bakil/Class_8_Methods/NegativeNumber.java
+++ src/main/java/org/la/student/bakil/Class_8_Methods/NegativeNumber.java
@@ -7,38 +7,21 @@ public class NegativeNumber {
 
 
 	public static void main(String[] args) {
-		int arr []= {1, 3,8,12,33,-11};
-		print(arr);
-		int pos [] = positive(arr);
-		print(pos);
-	}
-
-
-
-		static int [] positive(int x[]) {
-			int c[] = new int [x.length];
-			for (int i=0;i<x.length;i++) {
-				
-				if (x[i]>0) {
-					
+		int arr []= {-1, 3,8,12,33,-11};
+		printnegative (arr);
 		
-				}
-					
-				
-					
-				
-			}
-			return c;
-			
-			
-		}
-
-
-		 static void print(int[] arr) {
-		for (int a:arr ) {
-			System.out.println(a+" ");
-		}
+		
 		 }
+
+	 static void printnegative(int[] arr) {
+		for(int a:arr) {
+			if (a<0) {
+				System.out.println("The negative numbers are  : "+a);
+			}
+		}
+	//	System.out.println();
+		
+	}
 	}
 
 
--- src/main/java/org/la/student/bakil/Class_8_Methods/PositiveNumber.java
+++ src/main/java/org/la/student/bakil/Class_8_Methods/PositiveNumber.java
@@ -7,31 +7,20 @@ public class PositiveNumber {
 
 
 	public static void main(String[] args) {
-		int arr []= {1, 3,8,12,33,-11};
-
-		print(arr);
+		int arr []= {-1, 3,8,12,33,-11};
+		printpositive (arr);
 		
+		
+		 }
 
+	 static void printpositive(int[] arr) {
+		for(int a:arr) {
+			if (a>0) {
+				System.out.println("The positive numbers are  : "+a);
+			}
+		}
+	//	System.out.println();
+		
 	}
-
-		
-		static int [] pos (int s[]) {
-			int t[]=new int [s.length];
-			for (int i=0;i<s.length;i++) {
-				if (s[i]<0) {
-				
-				}
-			
-			}
-			return t;
-				}
-	
-		static void print (int arr[]) {
-			for (int a:arr) {
-				System.out.println(a+" ");
-			}
-		}
-		}
-	
-
+	}
 
