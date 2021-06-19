e4c8508de6dcb51d1f350b590bb78d06bf4241c7
Tania <HP@LAPTOP-MDJKCEIE>
2021-06-19 21:21:43 +0800

    lecture-4

---
+++ src/main/java/org/la/student/tania_class4_task/Lec_4_Q1.java
@@ -0,0 +1,19 @@
+package org.la.student.tania_class4_task;
+
+public class Lec_4_Q1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//wap to find which number is greater among two numbers
+		int a=10;
+		int b=5;
+		if(a>b) {
+			System.out.println("the greatest number is" +a);
+		}
+		else
+		{
+			System.out.println("the greatest number is"+b);
+		}
+	}
+
+}
---
+++ src/main/java/org/la/student/tania_class4_task/Question_2.java
@@ -0,0 +1,22 @@
+package org.la.student.tania_class4_task;
+
+public class Question_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//wap to find which number is greater among three numbers
+int a=5;
+int b=10; int c=15;
+if(a>b && a>c) {
+	System.out.println("The greatest number is "+a);
+}
+else if(b>a&& b>c) {
+	System.out.println("The greatest number is "+b);
+}
+else {
+	System.out.println("The greatest number is "+c);
+}
+
+	}
+
+}
