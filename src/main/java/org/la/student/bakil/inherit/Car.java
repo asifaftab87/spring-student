558c40cee17a7f5b2c31605a3988fe1efd05ac0b
Bakil Alkhaledi <alkhaledi.khaled12@gmail.com>
2021-07-03 17:19:28 +0800

    inherit

--- src/main/java/org/la/student/bakil/inherit/Car.java
+++ src/main/java/org/la/student/bakil/inherit/Car.java
@@ -5,6 +5,7 @@ public class Car {
 	public void myCar() {
 		System.out.println("This is BMW car");
 	}
+	
 	private void vios () {
 		System.out.println("This is personal method ,can`t be access");
 	}
