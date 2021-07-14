b9badf6ef322b07b54b0b14ece479775784906d1
Aameen <Aameen@DATA-PC.domain.name>
2021-07-15 01:27:56 +0530

    All alphabet from a to z using while loop

---
+++ src/main/java/org/la/student/ameen/Checking26Alphabet.java
@@ -0,0 +1,23 @@
+package org.la.student.ameen;
+
+
+public class Checking26Alphabet {
+
+	public static void main(String[] args) {
+		// All alphabet from a to z using while loop
+		
+		char alpha = 'a' ;
+		System.out.println("All 26 alphabet ");
+		
+		while(alpha <= 'z')
+		{
+			System.out.println( "alpha : " +alpha);
+			alpha++;
+		}
+		
+		
+		
+
+	}
+
+}
