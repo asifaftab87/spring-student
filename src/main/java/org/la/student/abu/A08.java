249c068e82d8941a26960fb90061fcd90ae1da72
sajidali <sjdali819@gmail.com>
2021-09-03 01:14:36 +0530

    dome

--- src/main/java/org/la/student/abu/A08.java
+++ src/main/java/org/la/student/abu/A08.java
@@ -4,7 +4,7 @@ public class A08 {
 
 	public static void main(String[] args) {
 		int a = 8, b = 4, percentage;
-		percentage = (a/b)*100;
-		System.out.println("percentage of a and b is :"+percentage);
-	}}
-		
+		percentage = (a / b) * 100;
+		System.out.println("percentage of a and b is :" + percentage);
+	}
+}
